c574e7b017d1ff4033398dcb4ec9c150536a14aa
emjay0921 <michaelgonzales2117@gmail.com>
2022-08-30 16:18:25 +0800

    renamed indicators to domain, Individual prefix 'ind' to 'indv'

--- spp_demo/models/group.py
+++ spp_demo/models/group.py
@@ -28,89 +28,89 @@ class G2PGroup(models.Model):
     OK ➢ HHs with members that have chronic illness/medical conditions - extracted from
     vulnerability indicator
 """
-    z_crt_grp_num_children = fields.Integer(
+    z_ind_grp_num_children = fields.Integer(
         "Number of children",
-        compute="_compute_crt_grp_num_children",
+        compute="_compute_ind_grp_num_children",
         help="Households (HH) with children - extracted from demographic data of "
         "HH adult members plus child members",
         store=True,
     )
-    z_crt_grp_num_members = fields.Integer(
-        "Number of members", compute="_compute_crt_grp_num_members", store=True
+    z_ind_grp_num_members = fields.Integer(
+        "Number of members", compute="_compute_ind_grp_num_members", store=True
     )
-    z_crt_grp_num_adults = fields.Integer(
-        "Number of adults", compute="_compute_crt_grp_num_adults", store=True
+    z_ind_grp_num_adults = fields.Integer(
+        "Number of adults", compute="_compute_ind_grp_num_adults", store=True
     )
-    z_crt_grp_num_adults_woman = fields.Integer(
+    z_ind_grp_num_adults_woman = fields.Integer(
         "Number of adults woman",
-        compute="_compute_crt_grp_num_adults_woman",
+        compute="_compute_ind_grp_num_adults_woman",
         store=True,
     )
-    z_crt_grp_num_elderly = fields.Integer(
-        "Number of eldery", compute="_compute_crt_grp_num_eldery", store=True
+    z_ind_grp_num_elderly = fields.Integer(
+        "Number of eldery", compute="_compute_ind_grp_num_eldery", store=True
     )
 
-    z_crt_grp_is_hh_with_children = fields.Boolean(
+    z_ind_grp_is_hh_with_children = fields.Boolean(
         "Is household (HH) with children",
-        compute="_compute_crt_grp_is_hh_with_children",
+        compute="_compute_ind_grp_is_hh_with_children",
         store=True,
     )
 
-    # z_crt_grp_is_hh_with_disabled = fields.Boolean("HHs with disabled (mental or physical) members",
-    #                                                compute="_compute_crt_grp_is_hh_with_disabled")
+    # z_ind_grp_is_hh_with_disabled = fields.Boolean("HHs with disabled (mental or physical) members",
+    #                                                compute="_compute_ind_grp_is_hh_with_disabled")
 
-    z_crt_grp_is_hh_with_elderly = fields.Boolean(
+    z_ind_grp_is_hh_with_elderly = fields.Boolean(
         "Is household with elderly",
-        compute="_compute_crt_grp_is_hh_with_elderly",
+        compute="_compute_ind_grp_is_hh_with_elderly",
         help="Households (HH) with elderly (including single / elderly-headed "
         "HHs) - extracted from demographic data of HH adult members",
         store=True,
     )
 
-    z_crt_grp_is_hh_with_pregnant_lactating = fields.Boolean(
+    z_ind_grp_is_hh_with_pregnant_lactating = fields.Boolean(
         "Is household with pregnant/lactating women",
-        compute="_compute_crt_grp_is_hh_with_pregnant_lactating",
+        compute="_compute_ind_grp_is_hh_with_pregnant_lactating",
         help="HH with pregnant/lactating women",
         store=True,
     )
 
-    z_crt_grp_is_hh_with_disabled = fields.Boolean(
+    z_ind_grp_is_hh_with_disabled = fields.Boolean(
         "Is household disabled (mental or physical) members",
-        compute="_compute_crt_grp_is_hh_with_disabled",
+        compute="_compute_ind_grp_is_hh_with_disabled",
         help="HHs with disabled (mental or physical) members",
         store=True,
     )
 
-    z_crt_grp_is_hh_with_medical_condition = fields.Boolean(
+    z_ind_grp_is_hh_with_medical_condition = fields.Boolean(
         "Is household with members that have chronic illness/medical conditions",
-        compute="_compute_crt_grp_is_hh_with_medical_condition",
+        compute="_compute_ind_grp_is_hh_with_medical_condition",
         help="HHs with members that have chronic illness/medical conditions",
         store=True,
     )
 
-    z_crt_grp_is_single_head_hh = fields.Boolean(
+    z_ind_grp_is_single_head_hh = fields.Boolean(
         "Is single-headed household",
-        compute="_compute_crt_grp_is_single_head_hh",
+        compute="_compute_ind_grp_is_single_head_hh",
         help="Single-headed HH - extracted from demographic data of "
         "HH adult members",
         store=True,
     )
-    z_crt_grp_is_woman_head_hh = fields.Boolean(
+    z_ind_grp_is_woman_head_hh = fields.Boolean(
         "Is female-headed household",
-        compute="_compute_crt_grp_is_woman_head_hh",
+        compute="_compute_ind_grp_is_woman_head_hh",
         help="Female-headed HH - extracted from demographic data of "
         "HH adult members",
         store=True,
     )
-    z_crt_grp_is_elderly_head_hh = fields.Boolean(
+    z_ind_grp_is_elderly_head_hh = fields.Boolean(
         "Is elderly-headed household",
-        compute="_compute_crt_grp_is_eldery_head_hh",
+        compute="_compute_ind_grp_is_eldery_head_hh",
         help="Elderly-headed HHs - "
         "extracted from demographic data of HH adult members",
         store=True,
     )
 
-    def _compute_crt_grp_num_children(self):
+    def _compute_ind_grp_num_children(self):
         """
         Households (HH) with children
         Returns:
@@ -118,53 +118,51 @@ class G2PGroup(models.Model):
         """
         now = datetime.datetime.now()
         children = now - relativedelta(years=CHILDREN_AGE_LIMIT)
-        indicators = [("birthdate", ">=", children)]
-        self.compute_count_and_set_indicator("z_crt_grp_num_children", None, indicators)
+        domain = [("birthdate", ">=", children)]
+        self.compute_count_and_set_indicator("z_ind_grp_num_children", None, domain)
 
-    def _compute_crt_grp_num_eldery(self):
+    def _compute_ind_grp_num_eldery(self):
         """
         Number of Eldery in this household
         Returns:
 
         """
         now = datetime.datetime.now()
-        indicators = [("birthdate", "<", now - relativedelta(years=ELDERLY_AGE_LIMIT))]
-        self.compute_count_and_set_indicator("z_crt_grp_num_elderly", None, indicators)
+        domain = [("birthdate", "<", now - relativedelta(years=ELDERLY_AGE_LIMIT))]
+        self.compute_count_and_set_indicator("z_ind_grp_num_elderly", None, domain)
 
-    def _compute_crt_grp_num_members(self):
+    def _compute_ind_grp_num_members(self):
         """
         Number of members in this household
         Returns:
 
         """
-        self.compute_count_and_set_indicator("z_crt_grp_num_members", None, [])
+        self.compute_count_and_set_indicator("z_ind_grp_num_members", None, [])
 
-    def _compute_crt_grp_num_adults(self):
+    def _compute_ind_grp_num_adults(self):
         """
         Number of adults in this household
         Returns:
 
         """
         now = datetime.datetime.now()
-        indicators = [("birthdate", "<", now - relativedelta(years=CHILDREN_AGE_LIMIT))]
-        self.compute_count_and_set_indicator("z_crt_grp_num_adults", None, indicators)
+        domain = [("birthdate", "<", now - relativedelta(years=CHILDREN_AGE_LIMIT))]
+        self.compute_count_and_set_indicator("z_ind_grp_num_adults", None, domain)
 
-    def _compute_crt_grp_num_adults_woman(self):
+    def _compute_ind_grp_num_adults_woman(self):
         """
         Number of adults woman in this household
         Returns:
 
         """
         now = datetime.datetime.now()
-        indicators = [
+        domain = [
             ("birthdate", "<", now - relativedelta(years=CHILDREN_AGE_LIMIT)),
             ("gender", "=", "Female"),
         ]
-        self.compute_count_and_set_indicator(
-            "z_crt_grp_num_adults_woman", None, indicators
-        )
+        self.compute_count_and_set_indicator("z_ind_grp_num_adults_woman", None, domain)
 
-    def _compute_crt_grp_is_single_head_hh(self):
+    def _compute_ind_grp_is_single_head_hh(self):
         """
         single-headed HH - extracted from demographic data of HH adult members
         Returns:
@@ -172,44 +170,44 @@ class G2PGroup(models.Model):
         """
         # TODO: Should we exclude eldery?
         now = datetime.datetime.now()
-        indicators = [("birthdate", "<", now - relativedelta(years=CHILDREN_AGE_LIMIT))]
+        domain = [("birthdate", "<", now - relativedelta(years=CHILDREN_AGE_LIMIT))]
         self.compute_count_and_set_indicator(
-            "z_crt_grp_is_single_head_hh", None, indicators, presence_only=True
+            "z_ind_grp_is_single_head_hh", None, domain, presence_only=True
         )
         # for record in self:
         #    if record["is_group"]:
-        #        cnt = record.count_individuals(kinds=None, indicators=indicators)
-        #        record["z_crt_grp_is_single_head_hh"] = cnt > 0
+        #        cnt = record.count_individuals(kinds=None, domain=domain)
+        #        record["z_ind_grp_is_single_head_hh"] = cnt > 0
         #    else:
-        #        record["z_crt_grp_is_single_head_hh"] = None
+        #        record["z_ind_grp_is_single_head_hh"] = None
 
-    def _compute_crt_grp_is_woman_head_hh(self):
+    def _compute_ind_grp_is_woman_head_hh(self):
         """
         female-headed HH - extracted from demographic data of HH adult members
         Returns:
 
         """
         _logger.info("-" * 80)
-        _logger.info("_compute_crt_grp_is_woman_head_hh")
+        _logger.info("_compute_ind_grp_is_woman_head_hh")
         _logger.info("self: %s", self)
         now = datetime.datetime.now()
-        indicators = [
+        domain = [
             ("birthdate", "<", now - relativedelta(years=CHILDREN_AGE_LIMIT)),
             ("gender", "=", "Female"),
         ]
         self.compute_count_and_set_indicator(
-            "z_crt_grp_is_woman_head_hh", None, indicators, presence_only=True
+            "z_ind_grp_is_woman_head_hh", None, domain, presence_only=True
         )
 
         # for record in self:
         #    if record["is_group"]:
-        #        cnt = record.count_individuals(kinds=["Head"], indicators=indicator)
+        #        cnt = record.count_individuals(kinds=["Head"], domain=indicator)
         #        _logger.info(cnt)
-        #        record["z_crt_grp_is_woman_head_hh"] = cnt > 0
+        #        record["z_ind_grp_is_woman_head_hh"] = cnt > 0
         #    else:
-        #        record["z_crt_grp_is_woman_head_hh"] = False
+        #        record["z_ind_grp_is_woman_head_hh"] = False
 
-    def _compute_crt_grp_is_eldery_head_hh(self):
+    def _compute_ind_grp_is_eldery_head_hh(self):
         """
         Elderly-headed HHs - extracted from demographic
         data of HH adult members
@@ -217,114 +215,114 @@ class G2PGroup(models.Model):
 
         """
         _logger.info("-" * 80)
-        _logger.info("_compute_crt_grp_is_eldery_head_hh")
+        _logger.info("_compute_ind_grp_is_eldery_head_hh")
         _logger.info("self: %s", self)
         now = datetime.datetime.now()
-        indicators = [("birthdate", "<", now - relativedelta(years=ELDERLY_AGE_LIMIT))]
+        domain = [("birthdate", "<", now - relativedelta(years=ELDERLY_AGE_LIMIT))]
         self.compute_count_and_set_indicator(
-            "z_crt_grp_is_elderly_head_hh", None, indicators, presence_only=True
+            "z_ind_grp_is_elderly_head_hh", None, domain, presence_only=True
         )
 
         # for record in self:
         #    if record["is_group"]:
-        #        cnt = record.count_individuals(kinds=["Head"], indicators=indicator)
+        #        cnt = record.count_individuals(kinds=["Head"], domain=indicator)
         #        _logger.info("cnt: %s", cnt)
-        #        record.z_crt_grp_is_elderly_head_hh = cnt > 0
+        #        record.z_ind_grp_is_elderly_head_hh = cnt > 0
         #    else:
-        #        record.z_crt_grp_is_elderly_head_hh = None
+        #        record.z_ind_grp_is_elderly_head_hh = None
 
-    def _compute_crt_grp_is_hh_with_children(self):
+    def _compute_ind_grp_is_hh_with_children(self):
         """
         Households (HH) with children - extracted from demographic data of HH adult members
         plus child members from personal data
         """
         now = datetime.datetime.now()
         children = now - relativedelta(years=CHILDREN_AGE_LIMIT)
-        indicators = [("birthdate", ">=", children)]
+        domain = [("birthdate", ">=", children)]
         self.compute_count_and_set_indicator(
-            "z_crt_grp_is_hh_with_children", None, indicators, presence_only=True
+            "z_ind_grp_is_hh_with_children", None, domain, presence_only=True
         )
 
         # for record in self:
         #    if record["is_group"]:
-        #        cnt = record.count_individuals(kinds=None, indicators=indicator)
-        #        record["z_crt_grp_is_hh_with_children"] = cnt > 0
+        #        cnt = record.count_individuals(kinds=None, domain=indicator)
+        #        record["z_ind_grp_is_hh_with_children"] = cnt > 0
         #    else:
-        #        record["z_crt_grp_is_hh_with_children"] = None
+        #        record["z_ind_grp_is_hh_with_children"] = None
 
-    def _compute_crt_grp_is_hh_with_pregnant_lactating(self):
+    def _compute_ind_grp_is_hh_with_pregnant_lactating(self):
         """
         Households (HH) with pregnant and lactating
         """
         datetime.datetime.now()
-        indicators = [
+        domain = [
             "|",
-            ("z_cst_ind_pregnancy_start", "!=", None),
-            ("z_cst_ind_lactation_start", "=", None),
+            ("z_cst_indv_pregnancy_start", "!=", None),
+            ("z_cst_indv_lactation_start", "=", None),
         ]
         self.compute_count_and_set_indicator(
-            "z_crt_grp_is_hh_with_pregnant_lactating",
+            "z_ind_grp_is_hh_with_pregnant_lactating",
             None,
-            indicators,
+            domain,
             presence_only=True,
         )
 
         # for record in self:
         #    if record["is_group"]:
-        #        cnt = record.count_individuals(kinds=None, indicators=indicator)
-        #        record.z_crt_grp_is_hh_with_pregnant_lactating = cnt > 0
+        #        cnt = record.count_individuals(kinds=None, domain=indicator)
+        #        record.z_ind_grp_is_hh_with_pregnant_lactating = cnt > 0
         #    else:
-        #        record.z_crt_grp_is_hh_with_pregnant_lactating = None
+        #        record.z_ind_grp_is_hh_with_pregnant_lactating = None
 
-    def _compute_crt_grp_is_hh_with_disabled(self):
+    def _compute_ind_grp_is_hh_with_disabled(self):
         """
         HHs with disabled (mental or physical) members
         """
-        indicators = [("z_cst_ind_disability_level", ">", 0)]
+        domain = [("z_cst_indv_disability_level", ">", 0)]
         self.compute_count_and_set_indicator(
-            "z_crt_grp_is_hh_with_disabled", None, indicators, presence_only=True
+            "z_ind_grp_is_hh_with_disabled", None, domain, presence_only=True
         )
 
         # for record in self:
         #    if record["is_group"]:
-        #        cnt = record.count_individuals(kinds=None, indicators=indicator)
-        #        record.z_crt_grp_is_hh_with_disabled = cnt > 0
+        #        cnt = record.count_individuals(kinds=None, domain=indicator)
+        #        record.z_ind_grp_is_hh_with_disabled = cnt > 0
         #    else:
-        #        record.z_crt_grp_is_hh_with_disabled = None
+        #        record.z_ind_grp_is_hh_with_disabled = None
 
-    def _compute_crt_grp_is_hh_with_medical_condition(self):
+    def _compute_ind_grp_is_hh_with_medical_condition(self):
         """
         HHs with members that have chronic illness/medical conditions
         """
-        indicators = [("z_cst_ind_medical_condition", ">", 0)]
+        domain = [("z_cst_indv_medical_condition", ">", 0)]
         self.compute_count_and_set_indicator(
-            "z_crt_grp_is_hh_with_medical_condition",
+            "z_ind_grp_is_hh_with_medical_condition",
             None,
-            indicators,
+            domain,
             presence_only=True,
         )
 
         # for record in self:
         #    if record["is_group"]:
-        #        cnt = record.count_individuals(kinds=None, indicators=indicator)
-        #        record.z_crt_grp_is_hh_with_medical_condition = cnt > 0
+        #        cnt = record.count_individuals(kinds=None, domain=indicator)
+        #        record.z_ind_grp_is_hh_with_medical_condition = cnt > 0
         #    else:
-        #        record.z_crt_grp_is_hh_with_medical_condition = None
+        #        record.z_ind_grp_is_hh_with_medical_condition = None
 
-    def _compute_crt_grp_is_hh_with_elderly(self):
+    def _compute_ind_grp_is_hh_with_elderly(self):
         """
         Households (HH) with elderly - extracted from demographic data of HH adult members
         plus elderly members from personal data
         """
         now = datetime.datetime.now()
-        indicators = [("birthdate", "<", now - relativedelta(years=ELDERLY_AGE_LIMIT))]
+        domain = [("birthdate", "<", now - relativedelta(years=ELDERLY_AGE_LIMIT))]
         self.compute_count_and_set_indicator(
-            "z_crt_grp_is_hh_with_elderly", None, indicators, presence_only=True
+            "z_ind_grp_is_hh_with_elderly", None, domain, presence_only=True
         )
 
         # for record in self:
         #    if record["is_group"]:
-        #        cnt = record.count_individuals(kinds=None, indicators=indicator)
-        #        record.z_crt_grp_is_hh_with_elderly = cnt > 0
+        #        cnt = record.count_individuals(kinds=None, domain=indicator)
+        #        record.z_ind_grp_is_hh_with_elderly = cnt > 0
         #    else:
-        #        record.z_crt_grp_is_hh_with_elderly = None
+        #        record.z_ind_grp_is_hh_with_elderly = None
