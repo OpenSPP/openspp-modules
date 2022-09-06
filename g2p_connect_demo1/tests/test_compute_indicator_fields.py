1dfadb646d37ffdd6b670a639a7045dd767ad8c7
emjay0921 <michaelgonzales2117@gmail.com>
2022-09-06 13:48:43 +0800

    [Modified] - additional test

--- g2p_connect_demo1/tests/test_compute_indicator_fields.py
+++ g2p_connect_demo1/tests/test_compute_indicator_fields.py
@@ -1,6 +1,9 @@
 # Part of OpenG2P Registry. See LICENSE file for full copyright and licensing details.
 
+import datetime
 import logging
+
+from dateutil.relativedelta import relativedelta
 
 from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
@@ -31,6 +34,7 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "is_registrant": True,
                 "gender": "Female",
                 "z_cst_indv_cyclone_aug_2022_injured": True,
+                "birthdate": datetime.datetime.now(),
             }
         )
         cls.registrant_2 = cls.env["res.partner"].create(
@@ -42,6 +46,7 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "is_registrant": True,
                 "gender": "Female",
                 "z_cst_indv_cyclone_aug_2022_injured": True,
+                "birthdate": datetime.datetime.now(),
             }
         )
 
@@ -55,6 +60,7 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "is_registrant": True,
                 "gender": "Female",
                 "z_cst_indv_disability_level": 5,
+                "birthdate": datetime.datetime.now(),
             }
         )
         cls.registrant_4 = cls.env["res.partner"].create(
@@ -66,6 +72,7 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "is_registrant": True,
                 "gender": "Male",
                 "z_cst_indv_disability_level": 10,
+                "birthdate": datetime.datetime.now(),
             }
         )
 
@@ -79,6 +86,7 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "is_registrant": True,
                 "gender": "Male",
                 "z_cst_indv_receive_government_benefits": True,
+                "birthdate": datetime.datetime.now(),
             }
         )
         cls.registrant_6 = cls.env["res.partner"].create(
@@ -90,6 +98,7 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "is_registrant": True,
                 "gender": "Female",
                 "z_cst_indv_receive_government_benefits": True,
+                "birthdate": datetime.datetime.now(),
             }
         )
 
@@ -101,8 +110,9 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "name": "Juan Dela Cruz",
                 "is_group": False,
                 "is_registrant": True,
-                "gender": "Female",
+                "gender": "Male",
                 "z_cst_indv_cyclone_aug_2022_lost_livestock": True,
+                "birthdate": datetime.datetime.now(),
             }
         )
         cls.registrant_8 = cls.env["res.partner"].create(
@@ -114,6 +124,7 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "is_registrant": True,
                 "gender": "Male",
                 "z_cst_indv_cyclone_aug_2022_lost_livestock": True,
+                "birthdate": datetime.datetime.now(),
             }
         )
 
@@ -127,6 +138,7 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "is_registrant": True,
                 "gender": "Female",
                 "z_cst_indv_cyclone_aug_2022_lost_primary_source_income": True,
+                "birthdate": datetime.datetime.now(),
             }
         )
         cls.registrant_10 = cls.env["res.partner"].create(
@@ -138,6 +150,7 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "is_registrant": True,
                 "gender": "Female",
                 "z_cst_indv_cyclone_aug_2022_lost_primary_source_income": True,
+                "birthdate": datetime.datetime.now(),
             }
         )
 
@@ -162,48 +175,186 @@ class ComputeIndicatorFieldsTest(TransactionCase):
                 "is_registrant": True,
             }
         )
-        members1 = {
-            cls.registrant_1.id,
-            cls.registrant_3.id,
-            cls.registrant_5.id,
-            cls.registrant_7.id,
-            cls.registrant_9.id,
-        }
-        group1_members = {"individual": members1}
-        cls.group_1.write({"group_membership_ids": [(0, 0, group1_members)]})
-        members2 = {
-            cls.registrant_2.id,
-            cls.registrant_4.id,
-            cls.registrant_6.id,
-            cls.registrant_8.id,
-            cls.registrant_10.id,
-        }
-        group2_members = {"individual": members2}
-        cls.group_2.write({"group_membership_ids": [(0, 0, group2_members)]})
+        members1 = []
+        members1.append({"individual": cls.registrant_2.id})
+        members1.append({"individual": cls.registrant_4.id})
+        members1.append({"individual": cls.registrant_6.id})
+        members1.append({"individual": cls.registrant_8.id})
+        members1.append({"individual": cls.registrant_10.id})
+        group1_members = []
+        for val in members1:
+            group1_members.append([0, 0, val])
+
+        cls.group_1.write({"group_membership_ids": group1_members})
+        members2 = []
+        members2.append({"individual": cls.registrant_1.id})
+        members2.append({"individual": cls.registrant_3.id})
+        members2.append({"individual": cls.registrant_5.id})
+        members2.append({"individual": cls.registrant_7.id})
+        members2.append({"individual": cls.registrant_9.id})
+        group2_members = []
+        for val in members2:
+            group2_members.append([0, 0, val])
+
+        cls.group_2.write({"group_membership_ids": group2_members})
 
     def test_01_num_children(self):
+        self.group_1._compute_ind_grp_num_children()
         self.assertEqual(
-            len(self.group_1.group_membership_ids),
+            self.group_1.z_ind_grp_num_children,
             5,
         )
+        self.group_2._compute_ind_grp_num_children()
         self.assertEqual(
-            len(self.group_2.group_membership_ids),
+            self.group_2.z_ind_grp_num_children,
             5,
         )
 
-    def test_02_assign_member(self):
-        self.registrant_2.write(
-            {"individual_membership_ids": [(0, 0, {"group": self.group_2.id})]}
+    def test_02_num_eldery(self):
+        now = datetime.datetime.now()
+        birthdate = now - relativedelta(years=66)
+
+        self.registrant_1.write({"birthdate": birthdate})
+        self.registrant_3.write({"birthdate": birthdate})
+        self.group_2._compute_ind_grp_num_eldery()
+        self.assertEqual(
+            self.group_2.z_ind_grp_num_elderly,
+            2,
         )
-        message = (
-            "Membership Testing: Assigning Member to Group Failed! Result %s Expecting %s"
-            % (
-                self.registrant_2.individual_membership_ids[0].group.id,
-                self.group_2.id,
-            )
+        self.registrant_2.write({"birthdate": birthdate})
+        self.group_1._compute_ind_grp_num_eldery()
+        self.assertEqual(
+            self.group_1.z_ind_grp_num_elderly,
+            1,
+        )
+
+    def test_03_num_adults_female_not_elderly(self):
+        now = datetime.datetime.now()
+        birthdate = now - relativedelta(years=20)
+
+        self.registrant_1.write({"birthdate": birthdate})
+        self.registrant_3.write({"birthdate": birthdate})
+        self.group_2._compute_ind_grp_num_adults_female_not_elderly()
+        self.assertEqual(
+            self.group_2.z_ind_grp_num_adults_female_not_elderly,
+            2,
+        )
+        self.registrant_2.write({"birthdate": birthdate})
+        self.group_1._compute_ind_grp_num_adults_female_not_elderly()
+        self.assertEqual(
+            self.group_1.z_ind_grp_num_adults_female_not_elderly,
+            1,
+        )
+
+    def test_04_num_adults_male_not_elderly(self):
+        now = datetime.datetime.now()
+        birthdate = now - relativedelta(years=20)
+
+        self.registrant_5.write({"birthdate": birthdate})
+        self.registrant_7.write({"birthdate": birthdate})
+        self.group_2._compute_ind_grp_num_adults_male_not_elderly()
+        self.assertEqual(
+            self.group_2.z_ind_grp_num_adults_male_not_elderly,
+            2,
+        )
+        self.registrant_4.write({"birthdate": birthdate})
+        self.group_1._compute_ind_grp_num_adults_male_not_elderly()
+        self.assertEqual(
+            self.group_1.z_ind_grp_num_adults_male_not_elderly,
+            1,
+        )
+
+    def test_05_num_cyclone_aug_2022_injured(self):
+        self.assertEqual(
+            self.group_2.z_ind_grp_num_cyclone_aug_2022_injured,
+            1,
         )
         self.assertEqual(
-            self.registrant_2.individual_membership_ids[0].group.id,
-            self.group_2.id,
-            message,
+            self.group_1.z_ind_grp_num_cyclone_aug_2022_injured,
+            1,
+        )
+
+    def test_06_num_receive_government_benefits(self):
+        self.assertEqual(
+            self.group_2.z_ind_grp_num_receive_government_benefits,
+            1,
+        )
+        self.assertEqual(
+            self.group_1.z_ind_grp_num_receive_government_benefits,
+            1,
+        )
+
+    def test_07_num_cyclone_aug_2022_lost_livestock(self):
+        self.assertEqual(
+            self.group_2.z_ind_grp_num_cyclone_aug_2022_lost_livestock,
+            1,
+        )
+        self.assertEqual(
+            self.group_1.z_ind_grp_num_cyclone_aug_2022_lost_livestock,
+            1,
+        )
+
+    def test_08_num_cyclone_aug_2022_lost_primary_source_income(self):
+        self.assertEqual(
+            self.group_2.z_ind_grp_num_cyclone_aug_2022_lost_primary_source_income,
+            1,
+        )
+        self.assertEqual(
+            self.group_1.z_ind_grp_num_cyclone_aug_2022_lost_primary_source_income,
+            1,
+        )
+
+    def test_09_num_disability(self):
+        self.assertEqual(
+            self.group_2.z_ind_grp_num_disability,
+            1,
+        )
+        self.assertEqual(
+            self.group_1.z_ind_grp_num_disability,
+            1,
+        )
+
+    def test_10_is_hh_with_disabled(self):
+        self.assertEqual(
+            self.group_2.z_ind_grp_is_hh_with_disabled,
+            True,
+        )
+        self.assertEqual(
+            self.group_1.z_ind_grp_is_hh_with_disabled,
+            True,
+        )
+
+    def test_11_is_single_head_hh(self):
+        self.assertEqual(
+            self.group_2.z_ind_grp_is_single_head_hh,
+            False,
+        )
+        self.assertEqual(
+            self.group_1.z_ind_grp_is_single_head_hh,
+            False,
+        )
+
+    def test_12_is_elderly_head_hh(self):
+        self.assertEqual(
+            self.group_2.z_ind_grp_is_elderly_head_hh,
+            False,
+        )
+        self.assertEqual(
+            self.group_1.z_ind_grp_is_elderly_head_hh,
+            False,
+        )
+
+    def test_13_num_cyclone_aug_2022_injured_disabled(self):
+        self.registrant_1.active = False
+        self.group_2._compute_ind_grp_num_cyclone_aug_2022_injured()
+        self.assertEqual(
+            self.group_2.z_ind_grp_num_cyclone_aug_2022_injured,
+            0,
+        )
+
+        self.registrant_2.active = False
+        self.group_1._compute_ind_grp_num_cyclone_aug_2022_injured()
+        self.assertEqual(
+            self.group_1.z_ind_grp_num_cyclone_aug_2022_injured,
+            0,
         )
