54e246e0bef6e13c252dcfeb5443e17db6b2a540
Edwin Gonzales <edwin@newlogic.com>
2022-10-03 20:06:17 +0800

    Added workflow

--- spp_change_request/models/types/source_mixin.py
+++ spp_change_request/models/types/source_mixin.py
@@ -1,5 +1,5 @@
 # Part of OpenG2P. See LICENSE file for full copyright and licensing details.
-from odoo import models
+from odoo import fields, models
 
 
 class ChangeRequestSourceMixin(models.AbstractModel):
@@ -7,6 +7,17 @@ class ChangeRequestSourceMixin(models.AbstractModel):
 
     _name = "spp.change.request.source.mixin"
     _description = "Change Request Data Source Mixin"
+    _rec_name = "registrant_id"
+
+    registrant_id = fields.Many2one(
+        "res.partner",
+        "Registrant",
+        domain=[("is_registrant", "=", True)],
+        required=True,
+    )
+    change_request_id = fields.Many2one(
+        "spp.change.request", "Change Request", required=True
+    )
 
     def get_request_type_view_id(self):
         """Retrieve form view."""
@@ -15,3 +26,13 @@ class ChangeRequestSourceMixin(models.AbstractModel):
             .search([("model", "=", self._name), ("type", "=", "form")], limit=1)
             .id
         )
+
+    def _get_name(self):
+        name = ""
+        if self.family_name:
+            name += self.family_name + ", "
+        if self.given_name:
+            name += self.given_name + " "
+        if self.addl_name:
+            name += self.addl_name + " "
+        return name.upper()
