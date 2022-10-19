457cd454241f2a8a23eb265d4295f667ee5e67c1
emjay0921 <michaelgonzales2117@gmail.com>
2022-10-19 11:39:52 +0800

    added test

---
+++ spp_area/tests/test_area.py
@@ -0,0 +1,41 @@
+# Part of OpenG2P Registry. See LICENSE file for full copyright and licensing details.
+
+import logging
+
+# from odoo.tests import tagged
+from odoo.tests.common import TransactionCase
+
+_logger = logging.getLogger(__name__)
+
+
+# @tagged("post_install", "-at_install")
+class AreaTest(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super(AreaTest, cls).setUpClass()
+
+        # Initial Setup of Variables
+        cls.area_1 = cls.env["spp.area"].create(
+            {
+                "name": "Testing Area",
+            }
+        )
+        cls.area_1_child = cls.env["spp.area"].create(
+            {
+                "name": "Testing Area Child",
+                "parent_id": cls.area_1.id,
+            }
+        )
+
+    def test_01_check_childs(self):
+        self.area_1._compute_get_childs()
+
+        self.assertEqual(len(self.area_1.child_ids), 1)
+
+    def test_02_check_complete_name(self):
+        self.area_1_child._compute_complete_name()
+
+        self.assertEqual(
+            self.area_1_child.complete_name,
+            "Testing Area > Testing Area Child",
+        )
