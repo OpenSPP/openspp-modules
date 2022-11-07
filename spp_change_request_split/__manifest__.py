984aa9bd9c4c3c07439fe67bb40d71c08cef4f88
Edwin Gonzales <edwin@newlogic.com>
2022-11-07 10:01:47 +0800

    Fix Split change type

---
+++ spp_change_request_split/__manifest__.py
@@ -0,0 +1,29 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+{
+    "name": "OpenSPP Change Request (Split)",
+    "category": "OpenSPP",
+    "version": "15.0.0.0.1",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/openspp-project/openspp-registry",
+    "license": "LGPL-3",
+    "development_status": "Alpha",
+    "maintainers": ["jeremi", "gonzalesedwin1123"],
+    "depends": [
+        "base",
+        "g2p_registry_base",
+        "g2p_registry_individual",
+        "g2p_registry_group",
+        "spp_change_request",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/change_request_split_view.xml",
+    ],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
---
+++ spp_change_request_split/models/change_request_split_validation_sequence.py
@@ -0,0 +1,15 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models
+
+
+class ChangeRequestSplitValidationSequence(models.Model):
+    _inherit = "spp.change.request.validation.sequence"
+
+    @api.model
+    def _selection_request_type_ref_id(self):
+        selection = super()._selection_request_type_ref_id()
+        new_request_type = ("spp.change.request.split", "Split Members")
+        if new_request_type not in selection:
+            selection.append(new_request_type)
+        return selection
