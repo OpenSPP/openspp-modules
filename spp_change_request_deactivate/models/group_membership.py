d5d54e7d91257ee869cce7adb2c7b0025a4e7814
Edwin Gonzales <edwin@newlogic.com>
2022-11-09 15:06:22 +0800

    Minor fix

--- spp_change_request_deactivate/models/group_membership.py
+++ spp_change_request_deactivate/models/group_membership.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class SPPGroupMembershipMoveTransfer(models.Model):
     _inherit = "spp.change.request.group.members"
 
-    group_deactivate_id = fields.Many2one("spp.change.request.move.transfer")
+    group_deactivate_id = fields.Many2one("spp.change.request.deactivate")
