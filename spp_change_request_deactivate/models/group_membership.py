538e65f6a3fe14609ad7d0d2da5a6a0e5aeb91d2
Edwin Gonzales <edwin@newlogic.com>
2022-11-11 16:04:49 +0800

    Removed change request types and transfer to PDS-SPP

--- spp_change_request_deactivate/models/group_membership.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class SPPGroupMembershipMoveTransfer(models.Model):
-    _inherit = "spp.change.request.group.members"
-
-    group_deactivate_id = fields.Many2one("spp.change.request.deactivate")
