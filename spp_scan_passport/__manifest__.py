75115f978e172d5896d6274b9dd65149cc183fc2
Redick Butay <red@Redicks-Air.lan>
2022-11-29 10:10:19 +0800

    able to update fields when calling scan passport api

---
+++ spp_scan_passport/__manifest__.py
@@ -0,0 +1,24 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+{
+    "name": "OpenSPP Registry - Scan Passport",
+    "category": "OpenSPP",
+    "version": "15.0.0.0.1",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/openspp/openspp-registry",
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "maintainers": ["jeremi", "gonzalesedwin1123"],
+    "depends": ["base", "g2p_registry_base"],
+    "data": ["views/registrant.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "spp_scan_passport/static/src/js/registrant.js",
+        ],
+    },
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
