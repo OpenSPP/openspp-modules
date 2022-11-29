a17bac48a6d057e2aed97f6d8c60c1ad0b35d74b
Jeremi Joslin <jeremi@newlogic.com>
2022-11-29 10:15:15 +0700

    Update __manifest__.py

--- spp_scan_passport/__manifest__.py
+++ spp_scan_passport/__manifest__.py
@@ -1,13 +1,13 @@
 # Part of OpenSPP. See LICENSE file for full copyright and licensing details.
 {
-    "name": "OpenSPP Registry - Scan Passport",
+    "name": "OpenSPP Registry - Scan ID Document",
     "category": "OpenSPP",
     "version": "15.0.0.0.1",
     "sequence": 1,
     "author": "OpenSPP.org",
     "website": "https://github.com/openspp/openspp-registry",
     "license": "LGPL-3",
-    "development_status": "Beta",
+    "development_status": "Alpha",
     "maintainers": ["jeremi", "gonzalesedwin1123"],
     "depends": ["base", "g2p_registry_base"],
     "data": ["views/registrant.xml"],
