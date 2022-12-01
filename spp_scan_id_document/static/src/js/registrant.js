95581777b0198dc96c717850c65c81971918f6d1
red <redick@newlogic.com>
2022-12-01 18:20:21 +0800

    fix multiple call of api on single click

--- spp_scan_id_document/static/src/js/registrant.js
+++ spp_scan_id_document/static/src/js/registrant.js
@@ -1,4 +1,4 @@
-odoo.define(function (require) {
+odoo.define("spp_scan_id_document.field", function (require) {
     const initialise_url = "http://localhost:12222/initialise";
     const readdocument_url = "http://localhost:12222/readdocument";
     const shutdown_url = "http://localhost:12222/shutdown";
@@ -15,24 +15,29 @@ odoo.define(function (require) {
         },
         init: function () {
             this._super.apply(this, arguments);
+            this.onclick_started = false;
         },
         _onClick: function () {
-            fetch(initialise_url, {
-                method: "GET",
-            }).then(() => {
-                fetch(readdocument_url, {
+            if (!this.onclick_started) {
+                this.onclick_started = true;
+                fetch(initialise_url, {
                     method: "GET",
-                })
-                    .then((read_response) => read_response.text())
-                    .then((response) => {
-                        this._setValue(response, "UPDATE");
-                        fetch(shutdown_url, {
-                            method: "GET",
-                        }).then(() => {
-                            // Shutdown completed
+                }).then(() => {
+                    fetch(readdocument_url, {
+                        method: "GET",
+                    })
+                        .then((read_response) => read_response.text())
+                        .then((response) => {
+                            this._setValue(response, "UPDATE");
+                            fetch(shutdown_url, {
+                                method: "GET",
+                            }).then(() => {
+                                // Shutdown completed
+                                this.onclick_started = false;
+                            });
                         });
-                    });
-            });
+                });
+            }
         },
     });
 
