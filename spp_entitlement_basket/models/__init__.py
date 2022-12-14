d952e93d3626e5a8603abc853c20b949972bb1b4
Edwin Gonzales <edwin@newlogic.com>
2022-12-14 18:16:57 +0800

    Fix error in unittest: import cycle

--- spp_entitlement_basket/models/__init__.py
+++ spp_entitlement_basket/models/__init__.py
@@ -1,6 +1,5 @@
 # Part of OpenSPP. See LICENSE file for full copyright and licensing details.
 
-from . import cycle
 from . import entitlement_manager
 from . import entitlement
 from . import stock
