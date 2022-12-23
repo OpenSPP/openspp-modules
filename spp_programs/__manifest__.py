9538bf5264f87c32a02bc7d1e20202e96b37046f
Edwin Gonzales <edwin@newlogic.com>
2022-12-23 10:26:07 +0800

    Fix Cycle UI->Entitlements table

--- spp_programs/__manifest__.py
+++ spp_programs/__manifest__.py
@@ -22,6 +22,7 @@
         "views/main_view.xml",
         "views/entitlement_view.xml",
         "views/cycle_view.xml",
+        "views/programs_view.xml",
         "views/service_point_view.xml",
     ],
     "assets": {},
