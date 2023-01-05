94424f7ad5a578f55e68fe4cb6c34823f6d3ab37
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2023-01-05 18:38:35 +0700

    [REM] remove module spp_demo

--- spp_photo/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
-
-from . import registrant
-from . import photo_category
-from . import image
-
-# from . import datasource
