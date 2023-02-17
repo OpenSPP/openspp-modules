5b9c9978822c5584cc3ceef3d315e5f89bf0d6cb
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2023-02-17 16:45:11 +0700

    [ADD] new module integrate between custom fields & custom filter

---
+++ spp_custom_field_custom_filter/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "SPP Custom field-Custom filter Integrate",
+    "version": "15.0.0.0.0",
+    "author": "OpenSPP.org",
+    "website": "https://github.com/openspp/openspp-registry",
+    "license": "LGPL-3",
+    "category": "",
+    "depends": [
+        "spp_custom_fields_ui",
+        "spp_custom_filter",
+    ],
+    "data": [
+        "views/ir_model_fields_views.xml",
+    ],
+    "auto_install": True,
+    "application": False,
+}
