2e2863a40b3e67b3206d18cd2c4c9caf133f8b8a
Edwin Gonzales <edwin@newlogic.com>
2023-02-22 22:34:08 +0800

    Fix DMS File error in dialog window

--- spp_change_request/models/dms_file.py
+++ spp_change_request/models/dms_file.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import _, models
 
 
 class SPPDMSFileCustom(models.Model):
@@ -6,3 +6,28 @@ class SPPDMSFileCustom(models.Model):
 
     def action_save_and_close(self):
         return {"type": "ir.actions.act_window_close"}
+
+    def action_attach_documents(self):
+        for rec in self:
+            form_id = self.env.ref(
+                "spp_change_request.view_dms_file_spp_custom_form"
+            ).id
+            action = {
+                "type": "ir.actions.act_window",
+                "view_mode": "form",
+                "view_id": form_id,
+                "view_type": "form",
+                "res_model": "dms.file",
+                "target": "new",
+                "res_id": rec.id,
+            }
+            dms_context = {
+                "category_readonly": True,
+            }
+            action.update(
+                {
+                    "name": _("Upload Document: %s", rec.category_id.name),
+                    "context": dms_context,
+                }
+            )
+            return action
