42b7cf1a3dcd12c920e1bab114961bab45d4b123
Jeremi Joslin <jeremi@newlogic.com>
2023-02-24 10:47:55 +0700

    update manifests to 15.0.1.0.4

--- spp_custom_fields_ui/__manifest__.py
+++ spp_custom_fields_ui/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "OpenSPP Custom Fields UI",
     "category": "OpenSPP",
-    "version": "15.0.0.0.0",
+    "version": "15.0.1.0.4",
     "sequence": 1,
     "author": "OpenSPP.org",
     "website": "https://github.com/openspp/openspp-registry",
