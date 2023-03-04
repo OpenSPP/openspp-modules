69f99896872aed39cd43b579c59e2a8b3ded936c
Jeremi Joslin <jeremi@newlogic.com>
2023-03-05 00:02:27 +0700

    update manifests to 15.0.1.0.12

--- spp_idpass/__manifest__.py
+++ spp_idpass/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "ID PASS",
     "category": "OpenSPP",
-    "version": "15.0.0.0.0",
+    "version": "15.0.1.0.12",
     "sequence": 1,
     "author": "OpenSPP.org",
     "website": "https://github.com/openspp/openspp-registry",
