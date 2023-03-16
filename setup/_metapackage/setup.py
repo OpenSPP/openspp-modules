ecc565ccb8da502ffc7e35321fc5d158210e1f75
Jeremi Joslin <jeremi@newlogic.com>
2023-03-16 23:32:37 +0700

    [ADD] setup.py

--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
         'odoo-addon-spp_custom_field_custom_filter>=15.0dev,<15.1dev',
         'odoo-addon-spp_custom_fields_ui>=15.0dev,<15.1dev',
         'odoo-addon-spp_custom_filter>=15.0dev,<15.1dev',
+        'odoo-addon-spp_data_export>=15.0dev,<15.1dev',
         'odoo-addon-spp_event_data>=15.0dev,<15.1dev',
         'odoo-addon-spp_event_demo>=15.0dev,<15.1dev',
         'odoo-addon-spp_idpass>=15.0dev,<15.1dev',
