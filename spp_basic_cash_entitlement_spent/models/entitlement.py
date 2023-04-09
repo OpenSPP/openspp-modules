53a8b74e2b3fb6bf27697ec324f454a59eb9f474
Jeremi Joslin <jeremi@newlogic.com>
2023-04-09 15:20:21 +0000

    fix style

--- spp_basic_cash_entitlement_spent/models/entitlement.py
+++ spp_basic_cash_entitlement_spent/models/entitlement.py
@@ -9,7 +9,9 @@ _logger = logging.getLogger(__name__)
 class G2PBasicEntitlementCashSpent(models.Model):
     _name = "g2p.entitlement"
 
-    spent_amount = fields.Monetary(required=True, currency_field="currency_id", default=0.0)
+    spent_amount = fields.Monetary(
+        required=True, currency_field="currency_id", default=0.0
+    )
 
     def _compute_balance(self):
         for rec in self:
