21252d99a420cbe73e8d0d6d81fe122ded72c445
eMJay0921 <michael@newlogic.com>
2023-04-19 08:43:53 +0000

    add additional details to the wizard

--- spp_registry_wizard/wizard/create_member_wizard.py
+++ spp_registry_wizard/wizard/create_member_wizard.py
@@ -1,9 +1,11 @@
 # Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+import json
 import logging
 import re
 from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
+from lxml import etree
 
 from odoo import Command, _, api, fields, models
 from odoo.exceptions import UserError
@@ -90,6 +92,7 @@ class OpenSPPCreateMemberWizard(models.TransientModel):
             if rec.reg_ids:
                 rec.insert_ids(member)
 
+            rec.update_custom_fields(member)
             member_vals = {
                 "group": rec.group_id.id,
                 "individual": member.id,
@@ -143,6 +146,17 @@ class OpenSPPCreateMemberWizard(models.TransientModel):
 
             if vals:
                 member.update({"reg_ids": vals})
+
+    def update_custom_fields(self, member):
+        wizard_model = self.env["ir.model"].search([("model", "=", self._name)])
+        wizard_custom_fields = self.env["ir.model.fields"].search(
+            [("model_id", "=", wizard_model.id), ("name", "like", "cst_indv")]
+        )
+        vals = {}
+        for custom_field in wizard_custom_fields:
+            vals[custom_field.name] = self[custom_field.name]
+
+        member.write(vals)
 
     def next_step(self):
         if self.state == "step1":
@@ -241,6 +255,184 @@ class OpenSPPCreateMemberWizard(models.TransientModel):
                     if head_member:
                         break
             rec.head_member = head_member
+
+    def check_custom_fields(self):
+        model = self.env["ir.model"].search([("model", "=", "res.partner")])
+        fields_name = self.env["ir.model.fields"].search(
+            [("model_id", "=", model.id), ("name", "like", "cst_indv")]
+        )
+
+        model_wizard = self.env["ir.model"].search([("model", "=", self._name)])
+        for field in fields_name:
+            field_exists = self.env["ir.model.fields"].search(
+                [("model_id", "=", model_wizard.id), ("name", "=", field.name)]
+            )
+
+            vals = {
+                "model_id": model_wizard.id,
+                "name": field.name,
+                "field_description": field.field_description,
+                "ttype": field.ttype,
+                "target_type": field.target_type,
+                "field_category": field.field_category,
+                "help": field.help,
+            }
+
+            if not field_exists:
+                self.env["ir.model.fields"].create(vals)
+            else:
+                field_exists.write(vals)
+
+    def fields_view_get(
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
+    ):
+        res = super(OpenSPPCreateMemberWizard, self).fields_view_get(
+            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu
+        )
+
+        self.check_custom_fields()
+
+        if view_type == "form":
+            doc = etree.XML(res["arch"])
+            basic_info_page = doc.xpath("//page[@name='basic_info']")
+
+            model_fields_id = self.env["ir.model.fields"].search(
+                [("model_id", "=", self._name)],
+                order="ttype, field_description",
+            )
+
+            if basic_info_page:
+                is_group = False
+                custom_page = etree.Element("page", {"string": "Additional Details"})
+                indicators_page = etree.Element("page", {"string": "Indicators"})
+
+                custom_div = etree.SubElement(
+                    custom_page, "div", {"class": "row mt16 o_settings_container"}
+                )
+                indicators_div = etree.SubElement(
+                    indicators_page, "div", {"class": "row mt16 o_settings_container"}
+                )
+                for rec in model_fields_id:
+                    els = rec.name.split("_")
+                    if len(els) >= 3 and (
+                        els[2] == "grp"
+                        and not is_group
+                        or els[2] == "indv"
+                        and is_group
+                    ):
+                        continue
+
+                    if len(els) >= 2 and els[1] == "cst":
+                        custom_div2 = etree.SubElement(
+                            custom_div,
+                            "div",
+                            {"class": "col-12 col-lg-6 o_setting_box"},
+                        )
+                        custom_div_left = etree.SubElement(
+                            custom_div2, "div", {"class": "o_setting_left_pane"}
+                        )
+                        custom_div_right = etree.SubElement(
+                            custom_div2, "div", {"class": "o_setting_right_pane"}
+                        )
+                        if rec.ttype == "boolean":
+                            etree.SubElement(
+                                custom_div_left, "field", {"name": rec.name}
+                            )
+                            etree.SubElement(
+                                custom_div_right, "label", {"for": rec.name}
+                            )
+                            if rec.help:
+                                custom_div_right_help = etree.SubElement(
+                                    custom_div_right, "div", {"class": "text-muted"}
+                                )
+                                span = etree.SubElement(custom_div_right_help, "span")
+                                span.text = rec.help
+
+                        else:
+                            etree.SubElement(
+                                custom_div_right, "label", {"for": rec.name}
+                            )
+
+                            if rec.help:
+                                custom_div_right_help = etree.SubElement(
+                                    custom_div_right, "div", {"class": "text-muted"}
+                                )
+                                span = etree.SubElement(custom_div_right_help, "span")
+                                span.text = rec.help
+
+                            custom_div_right_inner_div = etree.SubElement(
+                                custom_div_right, "div", {"class": "text-muted"}
+                            )
+                            etree.SubElement(
+                                custom_div_right_inner_div, "field", {"name": rec.name}
+                            )
+
+                    elif len(els) >= 2 and els[1] == "ind":
+                        indicators_div2 = etree.SubElement(
+                            indicators_div,
+                            "div",
+                            {"class": "col-12 col-lg-6 o_setting_box"},
+                        )
+                        indicators_div_left = etree.SubElement(
+                            indicators_div2, "div", {"class": "o_setting_left_pane"}
+                        )
+                        indicators_div_right = etree.SubElement(
+                            indicators_div2, "div", {"class": "o_setting_right_pane"}
+                        )
+                        if rec.ttype == "boolean":
+                            new_field = etree.SubElement(
+                                indicators_div_left,
+                                "field",
+                                {
+                                    "name": rec.name,
+                                    "readonly": "1",
+                                    "class": "oe_read_only",
+                                },
+                            )
+                            etree.SubElement(
+                                indicators_div_right, "label", {"for": rec.name}
+                            )
+                            if rec.help:
+                                indicators_div_right_help = etree.SubElement(
+                                    indicators_div_right, "div", {"class": "text-muted"}
+                                )
+                                span = etree.SubElement(
+                                    indicators_div_right_help, "span"
+                                )
+                                span.text = rec.help
+                        else:
+                            etree.SubElement(
+                                indicators_div_right, "label", {"for": rec.name}
+                            )
+                            if rec.help:
+                                indicators_div_right_help = etree.SubElement(
+                                    indicators_div_right, "div", {"class": "text-muted"}
+                                )
+                                span = etree.SubElement(
+                                    indicators_div_right_help, "span"
+                                )
+                                span.text = rec.help
+                            indicators_div_right_inner_div = etree.SubElement(
+                                indicators_div_right, "div", {"class": "text-muted"}
+                            )
+                            new_field = etree.SubElement(
+                                indicators_div_right_inner_div,
+                                "field",
+                                {"name": rec.name},
+                            )
+
+                        new_field.set("readonly", "1")
+                        modifiers = {"readonly": True}
+                        new_field.set("modifiers", json.dumps(modifiers))
+
+                if custom_div.getchildren():
+                    basic_info_page[0].addnext(custom_page)
+                if indicators_div.getchildren():
+                    basic_info_page[0].addnext(indicators_page)
+
+                res["arch"] = etree.tostring(doc, encoding="unicode")
+
+        return res
 
 
 class OpenSPPCreateMemberPhoneNumber(models.TransientModel):
