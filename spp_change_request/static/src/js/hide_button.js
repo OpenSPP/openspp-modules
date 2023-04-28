d2dc79f713f455003c28f8e08c3456c54b04d693
red <redick@newlogic.com>
2023-04-28 19:23:31 +0800

    improved code and comments; added else

--- spp_change_request/static/src/js/hide_button.js
+++ spp_change_request/static/src/js/hide_button.js
@@ -9,25 +9,24 @@ odoo.define("spp_change_request.view_change_request_form", function (require) {
         },
         updateButtons: function () {
             this._super.apply(this, arguments);
-
+            var data = this.renderer.state.data;
             if (this.getModelsToHide().includes(this.modelName)) {
                 // Only for models that needs to hide edit button
                 if (this.$buttons) {
                     // Check if button exists
-                    if (this.renderer.state.data.state !== "draft") {
+                    this.$buttons.find(".o_form_button_edit").toggleClass("o_hidden", false);
+                    if (data.state !== "draft") {
                         // Hide edit button if state is not draft
                         this.$buttons.find(".o_form_button_edit").toggleClass("o_hidden", true);
                     } else if (
                         !(
-                            (this.renderer.state.data.assign_to_id &&
-                                this.renderer.state.data.assign_to_id.data &&
-                                this.renderer.state.data.assign_to_id.data.id === user) ||
-                            (this.renderer.state.data.create_uid &&
-                                this.renderer.state.data.create_uid.data &&
-                                this.renderer.state.data.create_uid.data.id === user)
+                            (data.assign_to_id &&
+                                data.assign_to_id.data &&
+                                data.assign_to_id.data.id === user) ||
+                            (data.create_uid && data.create_uid.data && data.create_uid.data.id === user)
                         )
                     ) {
-                        // If current user is either in assign_to_id or create_uid then the edit button will hide
+                        // If current user is neither in assign_to_id or create_uid then the edit button will hide
                         this.$buttons.find(".o_form_button_edit").toggleClass("o_hidden", true);
                     }
                     var edit_mode = this.mode === "edit";
