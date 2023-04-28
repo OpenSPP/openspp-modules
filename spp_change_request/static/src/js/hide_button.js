00a07d7b33eaf507f53ae7395fa361a6560d5813
red <redick@newlogic.com>
2023-04-28 13:18:10 +0800

    improved code, added checker if state is not draft

--- spp_change_request/static/src/js/hide_button.js
+++ spp_change_request/static/src/js/hide_button.js
@@ -9,29 +9,31 @@ odoo.define("spp_change_request.view_change_request_form", function (require) {
         },
         updateButtons: function () {
             this._super.apply(this, arguments);
-            if (
-                this.$buttons &&
-                this.renderer.state.data.assign_to_id &&
-                this.renderer.state.data.assign_to_id.data &&
-                this.renderer.state.data.create_uid &&
-                this.renderer.state.data.create_uid.data
-            ) {
-                if (this.getModelsToHide().includes(this.modelName)) {
-                    if (
-                        this.renderer.state.data.state === "draft" &&
-                        this.renderer.state.data.assign_to_id.data.id !== user &&
-                        this.renderer.state.data.create_uid.data.id !== user
-                    ) {
+
+            if (this.getModelsToHide().includes(this.modelName)) {
+                // Only for models that needs to hide edit button
+                if (this.$buttons) {
+                    // Check if button exists
+                    if (this.renderer.state.data.state !== "draft") {
+                        // Hide edit button if state is not draft
                         this.$buttons.find(".o_form_button_edit").toggleClass("o_hidden", true);
-                    } else {
-                        this.$buttons.find(".o_form_button_edit").toggleClass("o_hidden", false);
+                    } else if (
+                        !(
+                            (this.renderer.state.data.assign_to_id &&
+                                this.renderer.state.data.assign_to_id.data &&
+                                this.renderer.state.data.assign_to_id.data.id === user) ||
+                            (this.renderer.state.data.create_uid &&
+                                this.renderer.state.data.create_uid.data &&
+                                this.renderer.state.data.create_uid.data.id === user)
+                        )
+                    ) {
+                        // If current user is either in assign_to_id or create_uid then the edit button will hide
+                        this.$buttons.find(".o_form_button_edit").toggleClass("o_hidden", true);
                     }
-                } else {
-                    this.$buttons.find(".o_form_button_edit").toggleClass("o_hidden", false);
+                    var edit_mode = this.mode === "edit";
+                    this.$buttons.find(".o_form_buttons_edit").toggleClass("o_hidden", !edit_mode);
+                    this.$buttons.find(".o_form_buttons_view").toggleClass("o_hidden", edit_mode);
                 }
-                var edit_mode = this.mode === "edit";
-                this.$buttons.find(".o_form_buttons_edit").toggleClass("o_hidden", !edit_mode);
-                this.$buttons.find(".o_form_buttons_view").toggleClass("o_hidden", edit_mode);
             }
         },
     });
