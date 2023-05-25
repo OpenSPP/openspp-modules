7772d29fdd3777c3ce98022d93b7f770277b8f6a
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2023-05-25 23:44:12 +0700

    [UPD] g2p_entitlement_cash: improve test coverage

---
+++ g2p_entitlement_cash/tests/__init__.py
@@ -0,0 +1,2 @@
+from . import test_create_program_wizard
+from . import test_entitlement_manager
---
+++ g2p_entitlement_cash/tests/test_entitlement_manager.py
@@ -0,0 +1,176 @@
+from datetime import date
+from unittest.mock import patch
+
+from odoo import fields
+from odoo.exceptions import UserError
+from odoo.tests import TransactionCase
+
+
+class TestEntitlementManager(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.registrants = self.env["res.partner"].create(
+            [
+                {
+                    "name": "Registrant 1 [TEST]",
+                    "is_registrant": True,
+                    "is_group": True,
+                },
+                {
+                    "name": "Registrant 2 [TEST]",
+                    "is_registrant": True,
+                    "is_group": True,
+                },
+            ]
+        )
+        self.program = self.env["g2p.program"].create(
+            {
+                "name": "Program 1 [TEST]",
+                "program_membership_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "partner_id": self.registrants[0].id,
+                            "state": "enrolled",
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "partner_id": self.registrants[-1].id,
+                            "state": "enrolled",
+                        },
+                    ),
+                ],
+            }
+        )
+        self.program.create_journal()
+        self.cycle = self.env["g2p.cycle"].create(
+            {
+                "name": "Cycle 1 [TEST]",
+                "program_id": self.program.id,
+                "start_date": fields.Date.today(),
+                "end_date": fields.Date.today(),
+            }
+        )
+        self._cash_entitlement_manager = self.env[
+            "g2p.program.entitlement.manager.cash"
+        ].create(
+            {
+                "name": "Entitlement Manager Cash 1 [TEST]",
+                "program_id": self.program.id,
+            }
+        )
+
+    def _funding(self):
+        return self.env["g2p.program.fund"].create(
+            {
+                "name": "PF-2023-000001",
+                "program_id": self.program.id,
+                "amount": 50_000_000.0,
+                "state": "posted",
+            }
+        )
+
+    def create_entitlement(self):
+        return self.env["g2p.entitlement"].create(
+            {
+                "partner_id": self.registrants[0].id,
+                "cycle_id": self.cycle.id,
+                "valid_from": fields.Date.today(),
+                "initial_amount": 5.0,
+            }
+        )
+
+    def test_01_prepare_entitlements(self):
+        with self.assertRaisesRegex(UserError, "no items entered for this"):
+            self._cash_entitlement_manager.prepare_entitlements(
+                self.cycle, self.program.program_membership_ids
+            )
+        self._cash_entitlement_manager.write(
+            {
+                "entitlement_item_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "amount": 5.0,
+                        },
+                    ),
+                ],
+            }
+        )
+        before_entitlement = self.env["g2p.entitlement"].search([])
+        self.assertFalse(before_entitlement.ids, "Start without entitlement!")
+        self._cash_entitlement_manager.prepare_entitlements(
+            self.cycle, self.program.program_membership_ids
+        )
+        after_entitlement = self.env["g2p.entitlement"].search([])
+        self.assertTrue(bool(after_entitlement.ids), "Entitlement should be created!")
+
+    def test_02_set_pending_validation_entitlements(self):
+        entitlement = self.create_entitlement()
+        entitlement.state = "draft"
+        self._cash_entitlement_manager.set_pending_validation_entitlements(self.cycle)
+        self.assertEqual(
+            entitlement.state,
+            "pending_validation",
+            "Entitlement now should be pending validation!",
+        )
+
+    @patch("odoo.fields.Date.today")
+    def test_03_validate_entitlements(self, mock_today):
+        mock_today.__name__ = "mock_today"
+        mock_today.return_value = date(2023, 5, 23)
+        entitlement = self.create_entitlement()
+        res = self._cash_entitlement_manager.validate_entitlements(self.cycle)
+        self.assertEqual(
+            res["params"]["type"],
+            "danger",
+            "Should display danger notification [no fund]!",
+        )
+        self._funding()
+        res = self._cash_entitlement_manager.validate_entitlements(self.cycle)
+        self.assertEqual(
+            res["params"]["type"], "success", "Should display success notification!"
+        )
+        self.assertEqual(
+            entitlement.state, "approved", "Entitlement should now approved!"
+        )
+        self.assertEqual(
+            entitlement.date_approved,
+            date(2023, 5, 23),
+            "Entitlement approving date should be today!",
+        )
+
+    def test_04_cancel_entitlements(self):
+        entitlement = self.create_entitlement()
+        self._cash_entitlement_manager.cancel_entitlements(self.cycle)
+        self.assertEqual(
+            entitlement.state, "cancelled", "Entitlement should now cancelled!"
+        )
+
+    def test_05_open_entitlements_form(self):
+        res = self._cash_entitlement_manager.open_entitlements_form(self.cycle)
+        for key in ["res_model", "type", "domain"]:
+            self.assertIn(
+                key, res.keys(), f"Key `{key}` is missing from return action!"
+            )
+        self.assertEqual(res["res_model"], "g2p.entitlement")
+        self.assertEqual(res["type"], "ir.actions.act_window")
+        self.assertEqual(res["domain"], [("cycle_id", "=", self.cycle.id)])
+
+    def test_06_open_entitlement_form(self):
+        entitlement = self.create_entitlement()
+        res = self._cash_entitlement_manager.open_entitlement_form(entitlement)
+        for key in ["res_model", "type", "target", "res_id", "view_mode"]:
+            self.assertIn(
+                key, res.keys(), f"Key `{key}` is missing from return action!"
+            )
+        self.assertEqual(res["res_model"], "g2p.entitlement")
+        self.assertEqual(res["type"], "ir.actions.act_window")
+        self.assertEqual(res["target"], "new")
+        self.assertEqual(res["res_id"], entitlement.id)
+        self.assertEqual(res["view_mode"], "form")
