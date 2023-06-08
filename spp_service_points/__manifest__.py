80653b9dbbfa11d4a5eae251145175df16966016
Jeremi Joslin <jeremi@newlogic.com>
2023-06-08 16:25:48 +0700

    update manifests to 15.0.1.0.17

--- spp_service_points/__manifest__.py
+++ spp_service_points/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "OpenSPP Service Points",
     "category": "OpenSPP",
-    "version": "15.0.0.0.0",
+    "version": "15.0.1.0.17",
     "sequence": 1,
     "author": "OpenSPP.org",
     "website": "https://github.com/openspp/openspp-registry",
