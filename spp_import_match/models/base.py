418e1d4e06c59a3b6a808b435017f08e387cdd4e
eMJay0921 <michael@newlogic.com>
2023-07-03 18:05:57 +0000

    move import match to base

---
+++ spp_import_match/models/base.py
@@ -0,0 +1,53 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+import re
+from odoo import api, models
+
+class Base(models.AbstractModel):
+    _inherit = "base"
+
+    @api.model
+    def load(self, fields, data):
+        usable, field_to_match = self.env["spp.import.match"]._usable_rules(self._name, fields)
+        if usable:
+            newdata = list()
+            if ".id" in fields:
+                column = fields.index(".id")
+                fields[column] = "id"
+                for values in data:
+                    dbid = int(values[column])
+                    values[column] = self.browse(dbid).get_external_id().get(dbid)
+            import_fields = list(map(models.fix_import_export_id_paths, fields))
+            converted_data = self._convert_records(
+                self._extract_records(import_fields, data)
+            )
+
+            if "id" not in fields:
+                fields.append("id")
+                import_fields.append(["id"])
+
+            clean_fields = [f[0] for f in import_fields]
+            for dbid, xmlid, record, info in converted_data:
+                row = dict(zip(clean_fields, data[info["record"]]))
+
+                match = self
+                if xmlid:
+                    row["id"] = xmlid
+                    newdata.append(tuple(row[f] for f in clean_fields))
+                    continue
+                elif dbid:
+                    match = self.browse(dbid)
+                else:
+                    match = self.env["spp.import.match"]._match_find(self, record, row)
+
+                flat_fields_to_remove = [item for sublist in field_to_match for item in sublist]
+                for fields_pop in flat_fields_to_remove:
+                    if fields_pop in fields:
+                        fields.remove(fields_pop)
+
+                match.export_data(fields)
+
+                ext_id = match.get_external_id()
+                row["id"] = ext_id[match.id] if match else row.get("id", "")
+                newdata.append(tuple(row[f] for f in fields))
+            data = newdata
+        return super(Base, self).load(fields, data)
---
+++ spp_import_match/tests/test_res_partner_import_match.py
@@ -0,0 +1,106 @@
+import logging
+from os import path
+
+from odoo.tests import TransactionCase
+
+_logger = logging.getLogger(__name__)
+
+PATH = path.join(path.dirname(__file__), "import_data", "%s.csv")
+OPTIONS = {
+    "headers": True,
+    "quoting": '"',
+    "separator": ",",
+}
+
+
+class TestResPartnerImportMatch(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self._test_hh = self.env["res.partner"].create(
+            {
+                "name": "Renaud",
+                "is_registrant": True,
+                "is_group": True,
+                "email": "renaud@gmail.com",
+            }
+        )
+        self._test_applicant = self.env["res.partner"].create(
+            {
+                "name": "Rufino Renaud",
+                "family_name": "Rufino",
+                "given_name": "Renaud",
+                "is_group": False,
+                "is_registrant": True,
+                "phone": "+639266716911",
+                "email": "rufinorenaud12@gmail.com",
+            }
+        )
+
+    def _base_import_record(self, res_model, file_name):
+        with open(PATH % file_name) as demo_file:
+            return self.env["base_import.import"].create(
+                {
+                    "res_model": res_model,
+                    "file": demo_file.read(),
+                    "file_name": "%s.csv" % file_name,
+                    "file_type": "csv",
+                }
+            )
+
+    def create_matching_given_family_name(self):
+        res_partner = self.env["ir.model"].search([("model", "=", "res.partner")])
+        vals = {"model_id": res_partner.id}
+        import_match = self.env["spp.import.match"].create(vals)
+        given_name_field = self.env["ir.model.fields"].search(
+            [("name", "=", "given_name"), ("model_id", "=", res_partner.id)]
+        )
+
+        self.env["spp.import.match.fields"].create(
+            {"field_id": given_name_field.id, "match_id": import_match.id}
+        )
+
+        family_name_field = self.env["ir.model.fields"].search(
+            [("name", "=", "family_name"), ("model_id", "=", res_partner.id)]
+        )
+
+        self.env["spp.import.match.fields"].create(
+            {"field_id": family_name_field.id, "match_id": import_match.id}
+        )
+
+        return import_match
+
+    def create_matching_name(self):
+        res_partner = self.env["ir.model"].search([("model", "=", "res.partner")])
+        vals = {"model_id": res_partner.id}
+        import_match = self.env["spp.import.match"].create(vals)
+        name_field = self.env["ir.model.fields"].search(
+            [("name", "=", "name"), ("model_id", "=", res_partner.id)]
+        )
+
+        self.env["spp.import.match.fields"].create(
+            {"field_id": name_field.id, "match_id": import_match.id}
+        )
+
+        return import_match
+
+    def test_01_res_partner_change_email_by_name(self):
+        """Change email based on given_name, family_name."""
+        self.create_matching_given_family_name()
+        record = self._base_import_record("res.partner", "res_partner_name")
+
+        record.execute_import(
+            ["given_name", "family_name", "name", "email"], [], OPTIONS
+        )
+
+        self._test_applicant.env.cache.invalidate()
+        self.assertEqual(self._test_applicant.email, "rufinorenaud@gmail.com")
+
+    def test_02_res_partner_change_email_by_group_name(self):
+        """Change email based on name."""
+        self.create_matching_name()
+        record = self._base_import_record("res.partner", "res_partner_group_name")
+
+        record.execute_import(["name", "email"], [], OPTIONS)
+
+        self._test_hh.env.cache.invalidate()
+        self.assertEqual(self._test_hh.email, "renaudhh@gmail.com")
