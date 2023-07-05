1dacf453dedfd8a2991979620ffdea9037654efe
eMJay0921 <michael@newlogic.com>
2023-07-05 03:26:12 +0000

    create base module to install minimum openspp module

---
+++ spp_base/__manifest__.py
@@ -0,0 +1,34 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+
+
+{
+    "name": "OpenSPP Base",
+    "category": "OpenSPP",
+    "version": "15.0.0.0.0",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/openspp/openspp-registry",
+    "license": "LGPL-3",
+    "development_status": "Production/Stable",
+    "maintainers": ["jeremi", "gonzalesedwin1123"],
+    "depends": [
+        "base",
+        "spp_area",
+        "spp_change_request",
+        "spp_event_data",
+        "spp_idpass",
+        "spp_idqueue",
+        "spp_service_points",
+        "spp_custom_field",
+        "spp_custom_fields_ui",
+        "spp_programs",
+    ],
+    "external_dependencies": {},
+    "data": [],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
