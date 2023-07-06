249ba97cc6275578a852507715c4c7af28d20451
red <redick@newlogic.com>
2023-07-06 10:13:02 +0800

    added spp_country_office module

---
+++ spp_country_office/__manifest__.py
@@ -0,0 +1,25 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+{
+    "name": "OpenSPP Country Office",
+    "category": "OpenSPP",
+    "version": "15.0.0.0.0",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/openspp/openspp-base",
+    "license": "LGPL-3",
+    "development_status": "Alpha",
+    "maintainers": ["jeremi", "gonzalesedwin1123"],
+    "depends": [
+        "base",
+        "g2p_registry_base",
+    ],
+    "data": [
+        "views/country_office_views.xml",
+    ],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
