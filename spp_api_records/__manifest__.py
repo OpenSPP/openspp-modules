bdc45ae605971dbeed7d47f27b87863571acf007
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2023-07-21 11:09:16 +0700

    [ADD] spp_api_records: new module

---
+++ spp_api_records/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": """OpenSPP REST API: API Records""",
+    "summary": """RESTful API routes for OpenSPP""",
+    "category": "",
+    "version": "15.0.0.0.0",
+    "application": False,
+    "author": "OpenSPP.org",
+    "development_status": "Alpha",
+    "website": "https://github.com/openspp/openspp-api",
+    "license": "LGPL-3",
+    "depends": ["spp_api"],
+    "data": [
+        
+    ],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
