4743e783668e5519f3b2cd88c18531d872f81ae7
red <redick@newlogic.com>
2023-08-03 12:54:24 +0800

    Revert "added special_create function to accomodate special creation of records of specific model"
    This reverts commit 29697bc747afea340bf984192d28f2ed880088ac.

--- spp_api/controllers/api.py
+++ spp_api/controllers/api.py
@@ -179,11 +179,7 @@ class ApiV1Controller(http.Controller):
         data = path.post_treatment_values(kw)
 
         records = self.get_records(path.model, kw)
-
-        if hasattr(records, "special_create"):
-            records.special_create(data)
-        else:
-            records.create(data)
+        records.create(data)
 
         response = {
             "time_stamp": str(datetime.datetime.now()),
