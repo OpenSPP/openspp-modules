3cdee0dbc922f6823138602bf8ef3d478e1712e3
red <redick@newlogic.com>
2023-08-16 14:46:34 +0800

    update code dependencies of spp_service_point_device

--- spp_api_records/__manifest__.py
+++ spp_api_records/__manifest__.py
@@ -10,8 +10,7 @@
     "depends": [
         "spp_api",
         "spp_service_points",
-        "spp_acf_service_point",
-        "spp_acf_service_point_device",
+        "spp_service_point_device",
     ],
     "data": [
         "data/spp_api_namespace_data.xml",
