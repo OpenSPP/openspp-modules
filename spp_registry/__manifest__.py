e5c22e4a845aaf034dc8a72323f8b6bb639c6c8c
red <redick@newlogic.com>
2023-08-22 11:49:17 +0800

    added smile audit for res partner

---
+++ spp_registry/__manifest__.py
@@ -0,0 +1,30 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "OpenSPP Registry",
+    "category": "OpenSPP",
+    "version": "15.0.0.0.0",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/openspp/openspp-registry",
+    "license": "LGPL-3",
+    "development_status": "Production/Stable",
+    "maintainers": ["jeremi", "gonzalesedwin1123"],
+    "depends": [
+        "base",
+        "g2p_registry_base",
+        "g2p_registry_group",
+        "g2p_registry_individual",
+        "smile_audit",
+    ],
+    "external_dependencies": {},
+    "data": [
+        "data/audit_rules_data.xml",
+    ],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
