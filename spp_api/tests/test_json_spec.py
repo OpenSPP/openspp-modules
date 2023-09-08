36d36ea7789c45344374c86c836c257adc8a03cc
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2023-09-08 14:58:13 +0700

    [FIX] test json spec

--- spp_api/tests/test_json_spec.py
+++ spp_api/tests/test_json_spec.py
@@ -2,35 +2,35 @@
 # Copyright 2018 Rafis Bikbov <https://it-projects.info/team/bikbov>
 # Copyright 2021 Denis Mudarisov <https://github.com/trojikman>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-import json
-
 from bravado_core.spec import Spec
 from swagger_spec_validator import SwaggerValidationError
 
 from odoo.tests import tagged
 from odoo.tests.common import HttpCase
-from odoo.tools import config
+from odoo.tools import config, mute_logger
 
 
 @tagged("post_install", "-at_install")
 class TestJsonSpec(HttpCase):
     def test_json_base(self):
-
+        db = self.env.cr.dbname
         resp = self.url_open(
-            "http://localhost:%d/api/v1/demo/swagger.json?token=demo_token&download"
-            % config["http_port"],
+            "http://localhost:%d/api/demo/v1/swagger.json?token=demo_token&db=%s"
+            % (config["http_port"], db),
             timeout=30,
         )
         self.assertEqual(resp.status_code, 200, "Cannot get json spec")
         # TODO add checking  actual content of the json
 
+    @mute_logger("py.warnings")
     def test_OAS_scheme_for_demo_data_is_valid(self):
+        db = self.env.cr.dbname
         resp = self.url_open(
-            "http://localhost:%d/api/v1/demo/swagger.json?token=demo_token&download"
-            % config["http_port"],
+            "http://localhost:%d/api/demo/v1/swagger.json?token=demo_token&db=%s"
+            % (config["http_port"], db),
             timeout=30,
         )
-        spec_dict = json.loads(resp.content.decode("utf8"))
+        spec_dict = resp.json()
         try:
             Spec.from_dict(spec_dict, config={"validate_swagger_spec": True})
         except SwaggerValidationError as e:
