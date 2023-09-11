fcc6f603c75163a381ccd4ac2850506cecac7a63
Edwin Gonzales <edwin@newlogic.com>
2023-09-11 13:26:48 +0800

    Change the way of calling the create_program function

--- spp_programs_sp/wizard/create_program_wizard.py
+++ spp_programs_sp/wizard/create_program_wizard.py
@@ -15,16 +15,12 @@ class SPPCreateNewProgramWiz(models.TransientModel):
     # Store beneficiary service points in entitlements
     store_sp_in_entitlements = fields.Boolean("Store Service Points to Entitlements")
 
-    def create_g2p_program(self):
-        # Create a new journal for this program
-        journal_id = self.create_journal(self.name, self.currency_id.id)
-
-        return self.env["g2p.program"].create(
+    def get_program_vals(self):
+        vals = super().get_program_vals()
+        vals.update(
             {
-                "name": self.name,
-                "journal_id": journal_id,
-                "target_type": self.target_type,
                 # Add the store_sp_in_entitlements field in program module
                 "store_sp_in_entitlements": self.store_sp_in_entitlements,
             }
         )
+        return vals
