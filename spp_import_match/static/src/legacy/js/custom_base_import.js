147c46c6d27db052e3e0861f9c012633dd7d3177
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2023-10-20 23:58:50 +0700

    [UPD] allow selecting multi config

--- spp_import_match/static/src/legacy/js/custom_base_import.js
+++ spp_import_match/static/src/legacy/js/custom_base_import.js
@@ -2,15 +2,17 @@ odoo.define("spp_import_match.import", function (require) {
     var rpc = require("web.rpc");
     var DataImport = require("base_import.import").DataImport;
 
-    function _make_option(term) {
-        return {id: term, text: term};
-    }
-
-    function _from_data(data, term) {
-        return _.findWhere(data, {id: term}) || _make_option(term);
-    }
-
     DataImport.include({
+        events: _.extend({}, DataImport.prototype.events, {
+            "change input.oe_import_match": "_onImportMatchChanges",
+        }),
+
+        init: function () {
+            this._super.apply(this, arguments);
+            this.importMatches = [];
+            this.configs = [];
+        },
+
         import_options: function () {
             var options = this._super.apply(this, arguments);
             options.use_queue = this.$("input.oe_import_queue").prop("checked");
@@ -19,14 +21,12 @@ odoo.define("spp_import_match.import", function (require) {
 
         onimported: function () {
             if (this.$("input.oe_import_queue").prop("checked")) {
-                console.log("CHECKED QUEUE");
                 this.displayNotification({
                     title: "Your request is being processed",
                     message: "You can check the status of this job in menu 'Queue / Jobs'.",
                     type: "success",
                 });
                 this.exit();
-                console.log("CHECKED QUEUE");
             } else {
                 this._super.apply(this, arguments);
             }
@@ -38,37 +38,39 @@ odoo.define("spp_import_match.import", function (require) {
 
         start: function () {
             var sup = this._super.apply(this, arguments);
-            console.log(this.res_model);
             this.setup_import_match_selection();
             return sup;
         },
 
         setup_import_match_selection: function () {
-            var configs = [];
-            var dataconfig = "_(configs)";
             var self = this;
 
             rpc.query({
                 model: "spp.import.match",
                 method: "search_read",
                 args: [[["model_id.model", "=", this.res_model]]],
+                fields: ["id", "name"],
             }).then(function (data) {
-                for (const [, value] of Object.entries(data)) {
-                    configs.push(value.name);
+                if (data.length <= 0) {
+                    self.$("div#oe_config_import_matching").hide();
+                    return;
                 }
-                dataconfig = _(configs);
-
-                const res = dataconfig.map(_make_option);
-                self.$("input.oe_import_match").select2({
-                    width: "100%",
-                    data: res,
-                    // Query: dataFilteredQuery,
-                    minimumResultsForSearch: -1,
-                    initSelection: function ($e, c) {
-                        c(_from_data(res, $e.val()) || _make_option($e.val()));
-                    },
-                });
+                var content = "";
+                for (const [, value] of Object.entries(data)) {
+                    self.configs.push(value);
+                    const id_for_label = "o_config_checkbox_import_match_" + value.id;
+                    content += `
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" id="${id_for_label}" class="custom-control-input oe_import_match"/>
+                            <label for="${id_for_label}" class="custom-control-label o_form_label">${value.name}</label>
+                        </div>`;
+                }
+                self.$("div#oe_import_match").html(content);
             });
+        },
+
+        _onImportMatchChanges: function () {
+            return;
         },
     });
 });
