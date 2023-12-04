28bcbe109ad4a7f3e158931b6401cbfb81e2e0be
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2023-12-04 10:26:02 +0700

    [UPD] update post init hook

--- spp_registrant_import/__init__.py
+++ spp_registrant_import/__init__.py
@@ -6,9 +6,7 @@ from . import models
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     contact_model = env.ref("base.model_res_partner")
-    registrant_id_field = env.ref(
-        "spp_registrant_import.field_res_partner__spp_id"
-    )
+    registrant_id_field = env.ref("spp_registrant_import.field_res_partner__spp_id")
     contact_import_match = env["spp.import.match"].search(
         [("model_id", "=", contact_model.id)]
     )
