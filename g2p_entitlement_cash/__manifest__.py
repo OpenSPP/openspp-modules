763c5d63e3db67e7d805d86a3010eb9a9f66231e
red <redick@newlogic.com>
2024-01-30 09:31:07 +0800

    [MIG] g2p_entitlement_cash: Migration to 17.0

--- g2p_entitlement_cash/__manifest__.py
+++ g2p_entitlement_cash/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "SPP Program Entitlement (Cash)",
     "category": "SPP",
-    "version": "15.0.1.1.0",
+    "version": "17.0.1.0.0",
     "sequence": 1,
     "author": "OpenSPP.org",
     "website": "https://github.com/openspp/openspp-modules",
