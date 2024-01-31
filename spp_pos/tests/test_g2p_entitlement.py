55633102c6a08422d8b69258b1ecbe9bccc6f6cb
red <redick@newlogic.com>
2024-01-31 22:24:38 +0800

    [MIG] spp_pos: Migration to 17.0

--- spp_pos/tests/test_g2p_entitlement.py
+++ spp_pos/tests/test_g2p_entitlement.py
@@ -3,16 +3,17 @@ from odoo.tests import TransactionCase
 
 
 class TestG2pEntitlement(TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.registrant = self.env["res.partner"].create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.registrant = cls.env["res.partner"].create(
             {
                 "name": "Registrant 1 [TEST]",
                 "is_registrant": True,
                 "is_group": True,
             }
         )
-        self.program = self.env["g2p.program"].create(
+        cls.program = cls.env["g2p.program"].create(
             {
                 "name": "Program 1 [TEST]",
                 "program_membership_ids": [
@@ -20,26 +21,26 @@ class TestG2pEntitlement(TransactionCase):
                         0,
                         0,
                         {
-                            "partner_id": self.registrant.id,
+                            "partner_id": cls.registrant.id,
                             "state": "enrolled",
                         },
                     ),
                 ],
             }
         )
-        self.program.create_journal()
-        self.cycle = self.env["g2p.cycle"].create(
+        cls.program.create_journal()
+        cls.cycle = cls.env["g2p.cycle"].create(
             {
                 "name": "Cycle 1 [TEST]",
-                "program_id": self.program.id,
+                "program_id": cls.program.id,
                 "start_date": fields.Date.today(),
                 "end_date": fields.Date.today(),
             }
         )
-        self._test_entitlement = self.env["g2p.entitlement"].create(
+        cls._test_entitlement = cls.env["g2p.entitlement"].create(
             {
-                "partner_id": self.registrant.id,
-                "cycle_id": self.cycle.id,
+                "partner_id": cls.registrant.id,
+                "cycle_id": cls.cycle.id,
                 "valid_from": fields.Date.today(),
                 "initial_amount": 5.0,
             }
