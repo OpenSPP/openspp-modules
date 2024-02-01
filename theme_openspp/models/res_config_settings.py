d76ed99bd9395e11f63747cbc9e5da12e7df9ae0
red <redick@newlogic.com>
2024-02-01 16:07:17 +0800

    commented _get_view for now since it may get us some errors in the UI

--- theme_openspp/models/res_config_settings.py
+++ theme_openspp/models/res_config_settings.py
@@ -4,20 +4,22 @@ from odoo import api, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    @api.model
-    def _get_view(self, view_id=None, view_type="form", **options):
-        arch, view = super()._get_view(view_id, view_type, **options)
+    # Commented (for now) since fields_view_get is now obsolete and replaced with _get_view with
+    # different return values
+    # @api.model
+    # def _get_view(self, view_id=None, view_type="form", **options):
+    #     arch, view = super()._get_view(view_id, view_type, **options)
 
-        page_name = view["name"]
+    #     page_name = view["name"]
 
-        if not page_name == "res.config.settings.view.form":
-            return arch, view
+    #     if not page_name == "res.config.settings.view.form":
+    #         return arch, view
 
-        doc = arch
+    #     doc = arch
 
-        query = "//div[div[field[@widget='upgrade_boolean']]]"
-        for item in doc.xpath(query):
-            item.attrib["class"] = "d-none"
+    #     query = "//div[div[field[@widget='upgrade_boolean']]]"
+    #     for item in doc.xpath(query):
+    #         item.attrib["class"] = "d-none"
 
-        arch = doc
-        return arch, view
+    #     arch = doc
+    #     return arch, view
