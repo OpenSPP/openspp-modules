a9fe2c13a64510b3c7d33e8e68a4b932a79c585d
emjay0921 <michael@newlogic.com>
2024-02-02 06:56:51 +0000

    add farmers form in the UI and override create and write functions of res_partner

--- spp_farmer_registry_base/models/farm.py
+++ spp_farmer_registry_base/models/farm.py
@@ -1,4 +1,9 @@
-from odoo import fields, models
+
+import logging
+
+from odoo import Command, fields, models, api
+
+_logger = logging.getLogger(__name__)
 
 
 class Farm(models.Model):
@@ -6,6 +11,7 @@ class Farm(models.Model):
     _inherits = {
         "spp.land.record": "farm_land_rec_id",
         "spp.farm.details": "farm_detail_id",
+        "spp.farmer": "farmer_id",
     }
 
     coordinates = fields.GeoPoint(string="GPS Coordinates")
@@ -40,8 +46,83 @@ class Farm(models.Model):
     farm_asset_id = fields.Many2one("spp.farm.asset", string="Farm Asset")
     farm_detail_id = fields.Many2one("spp.farm.details", string="Farm Detail")
     farm_land_rec_id = fields.Many2one("spp.land.record", string="Land Record")
+    farmer_id = fields.Many2one("spp.farmer", string="Farmer")
 
-    def create_farm_records(self):
-        for rec in self:
-            farm_land_rec_id = self.env["spp.land.record"].create({"farm_id": rec.id})
-            rec.farm_land_rec_id = farm_land_rec_id.id
+    @api.model
+    def create(self, vals):
+        farm = super(Farm, self).create(vals)
+        if farm.is_group:
+            self.create_update_farmer(farm)
+
+        return farm
+
+    @api.model
+    def write(self, vals):
+        farm = super(Farm, self).write(vals)
+        if self.is_group:
+            self.create_update_farmer(self)
+
+        return farm
+
+    def create_update_farmer(self, farm):
+        farmer_name = ""
+        if farm.farmer_family_name:
+            farmer_name += farm.farmer_family_name + ", "
+        if farm.farmer_given_name:
+            farmer_name += farm.farmer_given_name + " "
+        if farm.farmer_addtnl_name:
+            farmer_name += farm.farmer_addtnl_name
+
+        individual_vals = {
+            "family_name": farm.farmer_family_name,
+            "given_name": farm.farmer_given_name,
+            "name": farmer_name,
+            "addl_name": farm.farmer_addtnl_name or None,
+            "farmer_national_id": farm.farmer_national_id or None,
+            "gender": farm.farmer_sex or None,
+            "civil_status": farm.farmer_marital_status or None,
+            "birthdate": farm.farmer_birthdate or None,
+            "farmer_household_size": farm.farmer_household_size or None,
+            "farmer_postal_address": farm.farmer_postal_address or None,
+            "email": farm.farmer_email or None,
+            "formal_agricultural_training": farm.farmer_formal_agricultural or None,
+            "highest_education_level": farm.farmer_highest_education_level or None,
+            "is_registrant": True,
+            "is_group": False,
+        }
+        if not farm.farmer_individual_id:
+            individual = self.env["res.partner"].create(individual_vals)
+            farm.farmer_individual_id = individual.id
+            if farm.farmer_mobile_tel:
+                individual_phone_vals = {
+                    "partner_id": individual.id,
+                    "phone_no": farm.farmer_mobile_tel
+
+                }
+                self.env["g2p.phone.number"].create(individual_phone_vals)
+
+            # Create Membership
+            membership_vals = {
+                "group": farm.id,
+                "individual": individual.id,
+                "kind": [Command.link(self.env.ref("g2p_registry_membership.group_membership_kind_head").id)]
+            }
+            self.env["g2p.group.membership"].create(membership_vals)
+
+        else:
+            farm.farmer_individual_id.write(individual_vals)
+            if farm.farmer_mobile_tel:
+                current_phone = self.env["g2p.phone.number"].search(
+                    [
+                        ("partner_id", "=", farm.farmer_individual_id.id),
+                        ("phone_no", "=", farm.farmer_mobile_tel)
+                    ]
+                )
+                if not current_phone:
+                    individual_phone_vals = {
+                        "partner_id": farm.farmer_individual_id.id,
+                        "phone_no": farm.farmer_mobile_tel
+
+                    }
+                    self.env["g2p.phone.number"].create(individual_phone_vals)
+
--- spp_farmer_registry_base/models/farmer.py
+++ spp_farmer_registry_base/models/farmer.py
@@ -6,6 +6,9 @@ class Farmer(models.Model):
 
     experience_years = fields.Integer(string="Years of Experience")
     formal_agricultural_training = fields.Integer()
+    farmer_national_id = fields.Char(string="National ID Number")
+    farmer_household_size = fields.Char(string="Household Size")
+    farmer_postal_address = fields.Char("Postal Address")
     highest_education_level = fields.Selection(
         [
             ("none", "None"),
@@ -14,3 +17,34 @@ class Farmer(models.Model):
             ("tertiary", "Tertiary"),
         ],
     )
+
+class TempFarmer(models.Model):
+    _name = "spp.farmer"
+    _description = "Temporary Model for Farmer"
+
+    def _get_dynamic_selection(self):
+        options = self.env["gender.type"].search([])
+        return [(option.value, option.code) for option in options]
+
+    farmer_family_name = fields.Char(string="Farmer Family Name", required=True)
+    farmer_given_name = fields.Char(string="Farmer Given Name", required=True)
+    farmer_addtnl_name = fields.Char(string="Additional Name")
+    farmer_national_id = fields.Char(string="National ID Number")
+    farmer_mobile_tel = fields.Char(string="Mobile Telephone Number")
+    farmer_sex = fields.Selection(selection=_get_dynamic_selection, string="Sex")
+    farmer_marital_status = fields.Char(string="Marital Status")
+    farmer_birthdate = fields.Date("Date of Birth")
+    farmer_household_size = fields.Char(string="Household Size")
+    farmer_postal_address = fields.Char("Postal Address")
+    farmer_email = fields.Char("E-mail Address")
+    farmer_formal_agricultural = fields.Integer("Formal Agricultural Training")
+    farmer_highest_education_level = fields.Selection(
+        [
+            ("none", "None"),
+            ("primary", "Primary"),
+            ("secondary", "Secondary"),
+            ("tertiary", "Tertiary"),
+        ],
+        string="Highest Educational Level"
+    )
+    farmer_individual_id = fields.Many2one("res.partner")
