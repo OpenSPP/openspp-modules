98217dd4c37a64d6414217de493beb1e9276552e
Edwin Gonzales <edwin@newlogic.com>
2024-02-02 21:12:02 +0800

    Add theme_openspp_muk backend theme that extends MuK theme

---
+++ theme_openspp_muk/models/res_config_settings.py
@@ -0,0 +1,18 @@
+from odoo import models
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = "res.config.settings"
+
+    @property
+    def COLOR_ASSET_THEME_URL(self):
+        return "/theme_openspp_muk/static/src/scss/colors.scss"
+
+    @property
+    def COLOR_ASSET_LIGHT_URL(self):
+        return "/theme_openspp_muk/static/src/scss/colors_light.scss"
+
+    @property
+    def COLOR_ASSET_DARK_URL(self):
+        return "/theme_openspp_muk/static/src/scss/colors_dark.scss"
