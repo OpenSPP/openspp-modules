3c3fae0fb3bc5e3be6fad81f8689b4ebc11bef96
red <redick@newlogic.com>
2024-02-02 10:14:32 +0800

    fix issue on template

---
+++ spp_farmer_registry_base/models/partner.py
@@ -0,0 +1,9 @@
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    """Add geo_point to partner using a function filed"""
+
+    _inherit = "res.partner"
+
+    geo_point = fields.GeoPoint("Address coordinates")
