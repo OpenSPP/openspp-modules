f23c0902e57067d87d69b9371496a96fbcc2e0c5
Edwin Gonzales <edwin@newlogic.com>
2024-02-13 10:53:10 +0800

    Change sample dashboard

--- spp_farmer_registry_dashboard/reports/farmer_registry_report.py
+++ spp_farmer_registry_dashboard/reports/farmer_registry_report.py
@@ -27,6 +27,19 @@ class FarmerRegistryReport(models.Model):
         string="What percentage of your income comes from farming activities?",
         readonly=True,
     )
+    land_acreage = fields.Float()
+    details_legal_status = fields.Selection(
+        [
+            ("self", "Owned by self"),
+            ("family", "Owned by family"),
+            ("extended community", "Owned by extended community"),
+            ("cooperative", "Owned by cooperative"),
+            ("government", "Owned by Government"),
+            ("leased", "Leased from actual owner"),
+            ("unknown", "Do not Know"),
+        ],
+        string="Legal Status",
+    )
 
     @property
     def _table_query(self):
@@ -41,13 +54,16 @@ class FarmerRegistryReport(models.Model):
                 farm.registration_date AS registration_date,
                 dtl.financial_services_main_income_source AS financial_services_main_income_source,
                 dtl.financial_services_percentage_of_income_from_farming
-                    AS financial_services_percentage_of_income_from_farming
+                    AS financial_services_percentage_of_income_from_farming,
+                lr.land_acreage AS land_acreage,
+                dtl.details_legal_status AS details_legal_status
         """
 
     @api.model
     def _from(self):
         return """
             FROM res_partner AS farm
+                LEFT JOIN spp_land_record AS lr ON lr.id = farm.farm_land_rec_id
                 LEFT JOIN spp_farm_details AS dtl ON dtl.id = farm.farm_detail_id
         """
 
