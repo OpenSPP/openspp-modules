84ab612230eef3268f6dd64ff3e3177746194138
red <redick@newlogic.com>
2024-02-14 10:41:08 +0800

    [MIG] spp_dashboard: removed

--- spp_dashboard/__init__.py
+++
@@ -1,4 +0,0 @@
-# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
-
-from . import models
-from . import controllers
--- spp_dashboard/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
-
-from . import dashboard_menu
-from . import dashboard_block
-from . import programs
-from . import cycles
