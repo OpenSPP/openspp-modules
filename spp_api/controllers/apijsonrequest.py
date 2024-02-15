bcd7dc44bd6187e76048d8e65134f4a856309208
red <redick@newlogic.com>
2024-02-15 17:11:02 +0800

    [MIG] spp_api: Migrate to 17.0

--- spp_api/controllers/apijsonrequest.py
+++ spp_api/controllers/apijsonrequest.py
@@ -1,28 +1,10 @@
 # Copyright 2021 Denis Mudarisov <https://github.com/trojikman>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-import functools
-import json
 import logging
-import os
-import pprint
-import time
 
-import psycopg2
-import werkzeug.wrappers
-
-import odoo
 from odoo.http import (
-    Response,
-    Root,
-    SessionExpiredException,
-    WebRequest,
-    request,
-    rpc_request,
-    rpc_response,
-    serialize_exception,
+    Dispatcher,
 )
-from odoo.service.server import memory_info
-from odoo.tools import date_utils
 
 try:
     import psutil
@@ -33,192 +15,22 @@ except ImportError:
 _logger = logging.getLogger(__name__)
 
 
-class ApiJsonRequest(WebRequest):
-    _request_type = "http"
+class ApiJsonRequest(Dispatcher):
+    routing_type = "apijson"
 
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self, request):
+        super().__init__(request)
 
-        self.jsonp_handler = None
-        self.params = {}
+    @classmethod
+    def is_compatible_with(cls, request):
+        return True
 
-        args = self.httprequest.args
-        jsonp = args.get("jsonp")
-        self.jsonp = jsonp
-        request = None
-        request_id = args.get("id")
+    def dispatch(self, endpoint, args):
+        self.request.params = dict(**args, **self.request.get_http_params())
 
-        if jsonp and self.httprequest.method == "POST":
-            # jsonp 2 steps step1 POST: save call
-            def handler():
-                self.session[f"jsonp_request_{request_id}"] = self.httprequest.form["rb"]
-                self.session.modified = True
-                headers = [("Content-Type", "text/plain; charset=utf-8")]
-                r = werkzeug.wrappers.Response(request_id, headers=headers)
-                return r
-
-            self.jsonp_handler = handler
-            return
-        elif jsonp and args.get("rb"):
-            # jsonp method GET
-            request = args.get("rb")
-        elif jsonp and request_id:
-            # jsonp 2 steps step2 GET: run and return result
-            request = self.session.pop(f"jsonp_request_{request_id}", "{}")
+        if self.request.db:
+            result = self.request.registry["ir.http"]._dispatch(endpoint)
         else:
-            # regular jsonrpc2
-            request = self.httprequest.get_data().decode(self.httprequest.charset)
+            result = endpoint(**self.request.params)
 
-        # Read POST content or POST Form Data named "request"
-        try:
-            self.ApiJsonRequest = json.loads(request)
-        except ValueError as e:
-            msg = f"Invalid JSON data: {request!r}"
-            _logger.info("%s: %s", self.httprequest.path, msg)
-            raise werkzeug.exceptions.BadRequest(msg) from e
-
-        self.params = dict(self.ApiJsonRequest or {})
-        self.context = self.params.pop("context", dict(self.session.context))
-
-    def _json_response(self, result=None, error=None):
-        response = {}
-        if error is not None:
-            response["error"] = error
-
-        mime = "application/json"
-        status = error and error.pop("code") or result.status_code
-        body = response and json.dumps(response, default=date_utils.json_default) or result.data
-
-        return Response(
-            body,
-            status=status,
-            headers=[("Content-Type", mime), ("Content-Length", len(body))],
-        )
-
-    def _handle_exception(self, exception):
-        """Called within an except block to allow converting exceptions
-        to arbitrary responses. Anything returned (except None) will
-        be used as response."""
-        try:
-            return super()._handle_exception(exception)
-        except Exception:
-            if not isinstance(
-                exception,
-                odoo.exceptions.Warning
-                | SessionExpiredException
-                | odoo.exceptions.except_orm
-                | werkzeug.exceptions.NotFound,
-            ):
-                _logger.exception("Exception during JSON request handling.")
-            error = {
-                "code": 500,
-                "message": "Odoo Server Error",
-                "data": serialize_exception(exception),
-                "openapi_message": exception.args[0],
-            }
-            if isinstance(exception, psycopg2.InternalError):
-                error["message"] = "500: Internal Server Error"
-            if isinstance(exception, werkzeug.exceptions.NotFound):
-                error["code"] = 404
-                error["message"] = "404: Not Found"
-            if isinstance(exception, SessionExpiredException):
-                error["code"] = 100
-                error["message"] = "Odoo Session Expired"
-            return self._json_response(error=error)
-
-    def dispatch(self):
-        if self.jsonp_handler:
-            return self.jsonp_handler()
-        try:
-            rpc_request_flag = rpc_request.isEnabledFor(logging.DEBUG)
-            rpc_response_flag = rpc_response.isEnabledFor(logging.DEBUG)
-            if rpc_request_flag or rpc_response_flag:
-                endpoint = self.endpoint.method.__name__
-                model = self.params.get("model")
-                method = self.params.get("method")
-                args = self.params.get("args", [])
-
-                start_time = time.time()
-                start_memory = 0
-                if psutil:
-                    start_memory = memory_info(psutil.Process(os.getpid()))
-                if rpc_request and rpc_response_flag:
-                    rpc_request.debug("%s: %s %s, %s", endpoint, model, method, pprint.pformat(args))
-
-            result = self._call_function(**self.params)
-
-            if rpc_request_flag or rpc_response_flag:
-                end_time = time.time()
-                end_memory = 0
-                if psutil:
-                    end_memory = memory_info(psutil.Process(os.getpid()))
-                logline = "{}: {} {}: time:{:.3f}s mem: {}k -> {}k (diff: {}k)".format(
-                    endpoint,
-                    model,
-                    method,
-                    end_time - start_time,
-                    start_memory / 1024,
-                    end_memory / 1024,
-                    (end_memory - start_memory) / 1024,
-                )
-                if rpc_response_flag:
-                    rpc_response.debug("%s, %s", logline, pprint.pformat(result))
-                else:
-                    rpc_request.debug(logline)
-            return self._json_response(result)
-        except Exception as e:
-            return self._handle_exception(e)
-
-
-# Copy of http.route adding routing 'type':'api'
-def api_route(route=None, **kw):
-    routing = kw.copy()
-    assert "type" not in routing or routing["type"] in ("http", "json", "apijson")
-
-    def decorator(f):
-        if route:
-            if isinstance(route, list):
-                routes = route
-            else:
-                routes = [route]
-            routing["routes"] = routes
-
-        @functools.wraps(f)
-        def response_wrap(*args, **kw):
-            response = f(*args, **kw)
-            if isinstance(response, Response) or f.routing_type in ("apijson", "json"):
-                return response
-
-            if isinstance(response, bytes | str):
-                return Response(response)
-
-            if isinstance(response, werkzeug.exceptions.HTTPException):
-                response = response.get_response(request.httprequest.environ)
-            if isinstance(response, werkzeug.wrappers.BaseResponse):
-                response = Response.force_type(response)
-                response.set_default()
-                return response
-
-            _logger.warn(f"<function {f.__module__}.{f.__name__}> returns an invalid response type for an http request")
-            return response
-
-        response_wrap.routing = routing
-        response_wrap.original_func = f
-        return response_wrap
-
-    return decorator
-
-
-get_request_original = Root.get_request
-
-
-def api_get_request(self, httprequest):
-    # deduce type of request
-
-    if "authorization" in httprequest.headers and httprequest.headers.get("content-type", "") == "application/json":
-        return ApiJsonRequest(httprequest)
-
-    return get_request_original(self, httprequest)
-
-
-Root.get_request = api_get_request
+        return self.request.make_json_response(result.json, status=result.status)
--- spp_api_records/tests/test_g2p_entitlement.py
+++ spp_api_records/tests/test_g2p_entitlement.py
@@ -3,39 +3,40 @@ from odoo.tests import TransactionCase
 
 
 class TestG2pEntitlements(TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.test_group = self.env["res.partner"].create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.test_group = cls.env["res.partner"].create(
             {
                 "name": "group 1",
                 "is_registrant": True,
                 "is_group": True,
             }
         )
-        self.test_program_1 = self.env["g2p.program"].create(
+        cls.test_program_1 = cls.env["g2p.program"].create(
             {
                 "name": "program 1",
             }
         )
-        self.test_cycle_1 = self.env["g2p.cycle"].create(
+        cls.test_cycle_1 = cls.env["g2p.cycle"].create(
             {
                 "name": "cycle 1",
-                "program_id": self.test_program_1.id,
+                "program_id": cls.test_program_1.id,
                 "start_date": fields.Date.today(),
                 "end_date": fields.Date.add(fields.Date.today(), days=30),
             }
         )
-        self.test_entitlement = self.env["g2p.entitlement"].create(
+        cls.test_entitlement = cls.env["g2p.entitlement"].create(
             {
-                "cycle_id": self.test_cycle_1.id,
-                "partner_id": self.test_group.id,
+                "cycle_id": cls.test_cycle_1.id,
+                "partner_id": cls.test_group.id,
                 "initial_amount": 50_000,
             }
         )
-        self.test_entitlement_inkind = self.env["g2p.entitlement.inkind"].create(
+        cls.test_entitlement_inkind = cls.env["g2p.entitlement.inkind"].create(
             {
-                "cycle_id": self.test_cycle_1.id,
-                "partner_id": self.test_group.id,
+                "cycle_id": cls.test_cycle_1.id,
+                "partner_id": cls.test_group.id,
             }
         )
 
