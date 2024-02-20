b8bbc81bf8d544dbf86b967a37a1f2dc060ee070
Jeremi Joslin <jeremi@newlogic.com>
2024-02-20 10:57:44 +0700

    Add base GIS

---
+++ spp_base_gis/__manifest__.py
@@ -0,0 +1,21 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+{
+    "name": "OpenSPP Base GIS",
+    "category": "OpenSPP",
+    "version": "17.0.1.0.0",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/OpenSPP/openspp-modules",
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "maintainers": ["jeremi", "gonzalesedwin1123"],
+    "depends": [],
+    "external_dependencies": {"python": ["shapely", "pyproj", "geojson"]},
+    "data": [],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
