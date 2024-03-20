f09b9bbcf1706c9c3aa2c7b1fd5015cbe26f03ce
emjay0921 <michael@newlogic.com>
2024-03-20 07:21:43 +0000

    add g2p_registry_group in depends

--- spp_custom_field/__manifest__.py
+++ spp_custom_field/__manifest__.py
@@ -9,7 +9,7 @@
     "license": "LGPL-3",
     "development_status": "Beta",
     "maintainers": ["jeremi", "gonzalesedwin1123"],
-    "depends": ["base", "g2p_registry_base"],
+    "depends": ["base", "g2p_registry_base", "g2p_registry_group"],
     "data": [],
     "assets": {},
     "demo": [],
