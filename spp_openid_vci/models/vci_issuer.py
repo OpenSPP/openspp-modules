5ab28ed7dee7596317685b0fef4e94395e7f9b74
red <redick@newlogic.com>
2024-04-08 18:03:24 +0800

    fixed issue in signing and verifying the signed jwt

--- spp_openid_vci/models/vci_issuer.py
+++ spp_openid_vci/models/vci_issuer.py
@@ -11,3 +11,19 @@ class CustomOpenIDVCIssuer(models.Model):
                 args = [rec, f"set_default_auth_allowed_issuers_{rec.issuer_type}"]
                 if hasattr(*args):
                     getattr(*args)()
+
+    def sign_and_issue_credential(self, credential: dict) -> dict:
+        self.ensure_one()
+
+        ld_proof = self.build_empty_ld_proof()
+
+        signature = self.get_encryption_provider().jwt_sign(
+            {"credential": credential, "proof": ld_proof},
+            include_payload=False,
+            include_certificate=True,
+            include_cert_hash=True,
+        )
+        ld_proof["jws"] = signature
+        ret = dict(credential)
+        ret["proof"] = ld_proof
+        return ret
