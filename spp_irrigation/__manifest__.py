84183a98efc11e38f07a83403a33fa802ef2cefc
red <redick@newlogic.com>
2024-04-16 12:27:47 +0800

    created module spp_irrigation

---
+++ spp_irrigation/__manifest__.py
@@ -0,0 +1,29 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+
+
+{
+    "name": "OpenSPP: Land Record",
+    "category": "OpenSPP",
+    "version": "17.0.1.0.0",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/OpenSPP/openspp-modules",
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "maintainers": ["jeremi", "gonzalesedwin1123"],
+    "depends": [
+        "base",
+        "spp_base_gis",
+    ],
+    "data": [
+        "security/irrigation_security.xml",
+        "security/ir.model.access.csv",
+        "views/irrigation_view.xml",
+    ],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
