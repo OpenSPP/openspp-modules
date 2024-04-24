40a669b6012c282a7b65a712cabdc2a28bfa0181
red <redick@newlogic.com>
2024-04-24 18:53:17 +0800

    add reichie020212 as maintainer

--- spp_oauth/__manifest__.py
+++ spp_oauth/__manifest__.py
@@ -5,6 +5,7 @@
     "version": "17.0.1.0.0",
     "author": "OpenSPP.org",
     "development_status": "Alpha",
+    "maintainers": ["jeremi", "gonzalesedwin1123", "reichie020212"],
     "external_dependencies": {"python": ["pyjwt>=2.4.0"]},
     "website": "https://github.com/OpenSPP/openspp-modules",
     "license": "LGPL-3",
