68fe942954bac3d071f19704698c47a24a3950e5
Edwin Gonzales <edwin@newlogic.com>
2024-07-12 10:42:25 +0800

    Initial commit

---
+++ spp_advanced_targetting/__manifest__.py
@@ -0,0 +1,26 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+{
+    "name": "OpenSPP Targetting",
+    "category": "OpenSPP",
+    "version": "17.0.1.0.0",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/OpenSPP/openspp-modules",
+    "license": "LGPL-3",
+    "development_status": "Alpha",
+    "maintainers": ["jeremi", "gonzalesedwin1123"],
+    "depends": [
+        "base",
+        "g2p_registry_base",
+        "g2p_programs",
+    ],
+    "data": [
+    ],
+    "assets": {
+    },
+    "demo": [],
+    "images": [],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+}
