7bd8dab4919266e539bb0777e4221f9d7a13b0f7
red <redick@newlogic.com>
2024-07-23 13:58:55 +0800

    Added test cases for code coverage

---
+++ spp_area_gis/tests/test_area_import_raw.py
@@ -0,0 +1,35 @@
+import os
+
+from odoo.addons.spp_area.tests.test_area_import_raw import AreaImportRawTest as AreaImportRawTestMixin
+
+
+class AreaImportRawTest(AreaImportRawTestMixin):
+    @staticmethod
+    def get_file_path_1():
+        return f"{os.path.dirname(os.path.abspath(__file__))}/irq_adminboundaries_tabulardata.xlsx"
+
+    @staticmethod
+    def get_file_path_2():
+        return f"{os.path.dirname(os.path.abspath(__file__))}/pse_adminboundaries_tabulardata.xlsx"
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.area_import_raw_id.latitude = 13.0
+        cls.area_import_raw_id.longitude = 122.0
+
+    def test_check_errors(self):
+        self.area_import_raw_id.latitude = 91
+        self.area_import_raw_id.longitude = 181
+
+        self.area_import_raw_id.validate_raw_data()
+
+        self.assertEqual(self.area_import_raw_id.state, "Error")
+        self.assertIn("Latitude must be between -90 and 90", self.area_import_raw_id.remarks)
+        self.assertIn("Longitude must be between -180 and 180", self.area_import_raw_id.remarks)
+
+    def test_get_area_vals(self):
+        area_vals = self.area_import_raw_id.get_area_vals()
+
+        self.assertIn("coordinates", area_vals)
+        self.assertEqual(area_vals["coordinates"], '{"type": "Point", "coordinates": [122.0, 13.0]}')
--- spp_eligibility_sql/tests/test_create_program_wizard.py
+++ spp_eligibility_sql/tests/test_create_program_wizard.py
@@ -1,3 +1,4 @@
+from odoo.exceptions import UserError
 from odoo.tests import TransactionCase
 
 
@@ -5,6 +6,12 @@ class TestCreateProgramWiz(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.group_1 = cls.env["res.partner"].create(
+            {
+                "name": "Group 1",
+                "is_group": True,
+            }
+        )
         cls._program_create_wiz = cls.env["g2p.program.create.wizard"].create(
             {
                 "name": "Test Program 1 [SQL Elibigility]",
@@ -14,6 +21,8 @@ class TestCreateProgramWiz(TransactionCase):
                 "cycle_duration": 1,
                 "currency_id": cls.env.company.currency_id.id,
                 "entitlement_kind": "default",
+                "amount_per_cycle": 1.0,
+                "amount_per_individual_in_group": 1.0,
             }
         )
         cls.journal_id = cls._program_create_wiz.create_journal(
@@ -27,3 +36,52 @@ class TestCreateProgramWiz(TransactionCase):
             "recheck",
             "Correct SQL Query validation state",
         )
+
+    def test_02_test_sql_query(self):
+        self._program_create_wiz.test_sql_query()
+
+        self.assertEqual(self._program_create_wiz.sql_query_valid, "valid")
+        self.assertEqual(
+            self._program_create_wiz.sql_query_valid_message, "The SQL Query is valid but it did not return any record."
+        )
+        self.assertEqual(self._program_create_wiz.sql_record_count, 0)
+        self.assertEqual(self._program_create_wiz.state, "step1")
+
+        self._program_create_wiz.sql_query = "select id from res_partner"
+        self._program_create_wiz.test_sql_query()
+
+        self.assertEqual(self._program_create_wiz.sql_query_valid, "valid")
+        self.assertFalse(self._program_create_wiz.sql_query_valid_message)
+        self.assertEqual(self._program_create_wiz.sql_record_count, 1)
+        self.assertEqual(self._program_create_wiz.state, "step1")
+
+        self._program_create_wiz.target_type = "individual"
+        self._program_create_wiz.test_sql_query()
+
+        self.assertEqual(self._program_create_wiz.sql_query_valid, "valid")
+        self.assertEqual(
+            self._program_create_wiz.sql_query_valid_message, "The SQL Query is valid but it did not return any record."
+        )
+        self.assertEqual(self._program_create_wiz.sql_record_count, 0)
+        self.assertEqual(self._program_create_wiz.state, "step1")
+
+    def test_03_check_required_fields(self):
+        with self.assertRaisesRegex(UserError, "The SQL Query must return 1 or more record."):
+            self._program_create_wiz._check_required_fields()
+
+        with self.assertRaisesRegex(UserError, "The SQL Query must be validated first."):
+            self._program_create_wiz.sql_query_valid = False
+            self._program_create_wiz._check_required_fields()
+
+        with self.assertRaisesRegex(UserError, "A SQL Query is needed for this eligibility criteria type."):
+            self._program_create_wiz.sql_query = False
+            self._program_create_wiz._check_required_fields()
+
+    def test_04_get_eligibility_manager(self):
+        self._program_create_wiz.sql_query = "select id from res_partner"
+        self._program_create_wiz.sql_record_count = 1
+        self.program = self._program_create_wiz.create_program()
+        eligibility_manager = self._program_create_wiz._get_eligibility_manager(self.program["res_id"])
+
+        self.assertIn("eligibility_managers", eligibility_manager)
+        self.assertIn(4, eligibility_manager["eligibility_managers"][0])
