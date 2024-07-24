80d16ae8ddf778f1c10364ca8facd3a61ea5946e
red <redick@newlogic.com>
2024-07-24 12:14:19 +0800

    added test case for code coverage

--- spp_programs_sp/tests/__init__.py
+++ spp_programs_sp/tests/__init__.py
@@ -2,3 +2,4 @@ from . import common
 from . import test_entitlement_manager_cash
 from . import test_entitlement_manager_default
 from . import test_entitlement_manager_inkind
+from . import test_create_program_wizard
