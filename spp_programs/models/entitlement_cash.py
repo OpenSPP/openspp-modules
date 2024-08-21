ceb885760404d9a3381df8e8059bbeb89432c522
red <redick@newlogic.com>
2024-08-21 12:36:12 +0800

    Improved the validation of entitlements when approving

--- spp_programs/models/entitlement_cash.py
+++ spp_programs/models/entitlement_cash.py
@@ -1,9 +1,19 @@
 # Part of OpenSPP. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
+from odoo.exceptions import ValidationError
+
+from odoo.addons.g2p_programs.models.constants import STATE_APPROVED
 
 
 class CashEntitlement(models.Model):
     _inherit = "g2p.entitlement"
 
     id_number = fields.Char()
+
+    def approve_entitlement(self):
+        for rec in self:
+            if rec.cycle_id.state != STATE_APPROVED:
+                raise ValidationError("The cycle must be approved before approving entitlement")
+
+        return super().approve_entitlement()
