64102950545e8297a8214b152ab2fac7b26a297d
red <redick@newlogic.com>
2024-08-29 14:10:11 +0800

    Added test cases for code coverage

--- spp_farmer_registry_base/tests/test_farm.py
+++ spp_farmer_registry_base/tests/test_farm.py
@@ -1,3 +1,4 @@
+from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 
 
@@ -25,50 +26,47 @@ class MembershipTest(TransactionCase):
             }
         )
 
-    # Commented out because of errors when running in CI
-    # Need to further investigate since the error is not showing on local
+    def test_get_group_head_member(self):
+        head_id = self.farm_1.get_group_head_member()
+        self.assertTrue(head_id)
 
-    # def test_get_group_head_member(self):
-    #     head_id = self.farm_1.get_group_head_member()
-    #     self.assertTrue(head_id)
+        ind_head_id = self.registrant_1.get_group_head_member()
+        self.assertFalse(ind_head_id)
 
-    #     ind_head_id = self.registrant_1.get_group_head_member()
-    #     self.assertFalse(ind_head_id)
+    def test_write(self):
+        with self.assertRaisesRegex(ValidationError, "Farm must have a head member."):
+            self.farm_1.write({"group_membership_ids": [(2, self.farm_1.group_membership_ids.id)]})
 
-    # def test_write(self):
-    #     with self.assertRaisesRegex(ValidationError, "Farm must have a head member."):
-    #         self.farm_1.write({"group_membership_ids": [(2, self.farm_1.group_membership_ids.id)]})
+    def test_write_change_head(self):
+        def get_head_member(farm):
+            for member in self.farm_1.group_membership_ids:
+                if head_kind_id in member.kind:
+                    return member
 
-    # def test_write_change_head(self):
-    #     def get_head_member(farm):
-    #         for member in self.farm_1.group_membership_ids:
-    #             if head_kind_id in member.kind:
-    #                 return member
+        head_kind_id = self.env.ref("g2p_registry_membership.group_membership_kind_head")
 
-    #     head_kind_id = self.env.ref("g2p_registry_membership.group_membership_kind_head")
+        head_member = get_head_member(self.farm_1)
 
-    #     head_member = get_head_member(self.farm_1)
+        self.group_membership_1 = self.env["g2p.group.membership"].create(
+            {
+                "group": self.farm_1.id,
+                "individual": self.registrant_1.id,
+                "kind": [(6, 0, self.env.ref("g2p_registry_membership.group_membership_kind_head").ids)],
+            }
+        )
+        self.farm_1.write({"group_membership_ids": [(2, head_member.id)]})
 
-    #     self.group_membership_1 = self.env["g2p.group.membership"].create(
-    #         {
-    #             "group": self.farm_1.id,
-    #             "individual": self.registrant_1.id,
-    #             "kind": [(6, 0, self.env.ref("g2p_registry_membership.group_membership_kind_head").ids)],
-    #         }
-    #     )
-    #     self.farm_1.write({"group_membership_ids": [(2, head_member.id)]})
+        self.assertEqual(self.farm_1.farmer_family_name, self.registrant_1.family_name)
+        self.assertEqual(self.farm_1.farmer_given_name, self.registrant_1.given_name)
 
-    #     self.assertEqual(self.farm_1.farmer_family_name, self.registrant_1.family_name)
-    #     self.assertEqual(self.farm_1.farmer_given_name, self.registrant_1.given_name)
+    def test_head_info(self):
+        head_member_id = self.farm_1.get_group_head_member()
+        head_member_id.write(
+            {
+                "family_name": "Test Family Name",
+                "given_name": "Test Given Name",
+            }
+        )
 
-    # def test_head_info(self):
-    #     head_member_id = self.farm_1.get_group_head_member()
-    #     head_member_id.write(
-    #         {
-    #             "family_name": "Test Family Name",
-    #             "given_name": "Test Given Name",
-    #         }
-    #     )
-
-    #     self.assertEqual(self.farm_1.farmer_family_name, "Test Family Name")
-    #     self.assertEqual(self.farm_1.farmer_given_name, "Test Given Name")
+        self.assertEqual(self.farm_1.farmer_family_name, "Test Family Name")
+        self.assertEqual(self.farm_1.farmer_given_name, "Test Given Name")
