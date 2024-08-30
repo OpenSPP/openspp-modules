22d11ee061244af03746d6afe0846c0449d8b19c
Edwin Gonzales <edwin@newlogic.com>
2024-08-30 10:10:52 +0800

    Simplify the code for readability and optimization

--- spp_user_roles/models/res_partner.py
+++ spp_user_roles/models/res_partner.py
@@ -1,34 +1,23 @@
 # Part of OpenSPP. See LICENSE file for full copyright and licensing details.
 
-import logging
-
 from odoo import api, models
-
-_logger = logging.getLogger(__name__)
 
 
 class ResPartnerCustomSPP(models.Model):
     _inherit = "res.partner"
 
     @api.model
-    def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
-        _logger.debug("RES.PARTNER: search_read user: %s" % self.env.user.name)
-        if domain is None:
-            domain = []
-        if self.env.user.center_area_ids:
-            domain += [("area_id", "child_of", self.env.user.center_area_ids.ids)]
+    def _prepare_domain(self, domain):
+        domain = domain or []
+        domain += [("area_id", "child_of", self.env.user.center_area_ids.ids)] if self.env.user.center_area_ids else []
+        return domain
 
-        res = super().search_read(domain, fields, offset, limit, order)
-        _logger.debug("RES.PARTNER: search_read domain: %s" % domain)
-        return res
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
+        domain = self._prepare_domain(domain)
+        return super().search_read(domain, fields, offset, limit, order)
 
     @api.model
     def web_search_read(self, domain, specification, offset=0, limit=None, order=None, count_limit=None):
-        _logger.debug("RES.PARTNER: web_search_read user: %s" % self.env.user.name)
-        if domain is None:
-            domain = []
-        if self.env.user.center_area_ids:
-            domain += [("area_id", "child_of", self.env.user.center_area_ids.ids)]
-
-        _logger.debug("RES.PARTNER: web_search_read domain: %s" % domain)
+        domain = self._prepare_domain(domain)
         return super().web_search_read(domain, specification, offset, limit, order, count_limit)
