4501a44f20503c78ce0fe52132150e566b293ee0
red <redick@newlogic.com>
2024-09-04 12:44:28 +0800

    added test case for spp_starter

--- spp_starter/tests/test_spp_starter.py
+++ spp_starter/tests/test_spp_starter.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from odoo.tests import TransactionCase, tagged
 from odoo.tools.safe_eval import safe_eval
 
@@ -76,25 +78,41 @@ class TestSppStarter(TransactionCase):
         self.test_record._remove_default_products_if_needed()
         self.assertFalse(fake_product_template.active)
 
-    # def test_07_adjust_main_company_details(self):
-    #     if "account.move.line" in self.env:
-    #         self.env["account.move.line"].search([]).with_context(force_delete=True).unlink()
-    #     self.test_record._adjust_main_company_details()
-    #     company = self.env.company
-    #     self.assertEqual(company.name, self.test_record.org_name)
-    #     self.assertEqual(company.street, self.test_record.org_address)
-    #     self.assertEqual(company.phone, self.test_record.org_phone)
-    #     self.assertEqual(company.currency_id, self.test_record.org_currency_id)
+        self.test_record.conducting_inkind_transfer = "no"
+        result = self.test_record._remove_default_products_if_needed()
+        self.assertIsNone(result)
 
-    # def test_08_install_modules(self):
-    #     def find_module(module_name):
-    #         return self.env.ref(f"base.module_{module_name}", raise_if_not_found=False)
+    def test_07_adjust_main_company_details(self):
+        if "account.move.line" in self.env:
+            self.env["account.move.line"].search([]).with_context(force_delete=True).unlink()
+        self.test_record._adjust_main_company_details()
+        company = self.env.company
+        self.assertEqual(company.name, self.test_record.org_name)
+        self.assertEqual(company.street, self.test_record.org_address)
+        self.assertEqual(company.phone, self.test_record.org_phone)
+        self.assertEqual(company.currency_id, self.test_record.org_currency_id)
 
-    #     spp_theme = find_module("theme_openspp_muk")
-    #     self.assertIn(spp_theme, self.test_record._install_modules())
-    #     gp2_individual = find_module("g2p_registry_individual")
-    #     gp2_group = find_module("g2p_registry_group")
-    #     self.test_record.managing_target = "group"
-    #     self.assertIn(gp2_group, self.test_record._install_modules())
-    #     self.test_record.managing_target = "individual"
-    #     self.assertIn(gp2_individual, self.test_record._install_modules())
+    def test_08_install_modules(self):
+        def find_module(module_name):
+            return self.env.ref(f"base.module_{module_name}", raise_if_not_found=False)
+
+        spp_theme = find_module("theme_openspp_muk")
+        self.assertIn(spp_theme, self.test_record._install_modules())
+        gp2_individual = find_module("g2p_registry_individual")
+        gp2_group = find_module("g2p_registry_group")
+        self.test_record.managing_target = "group"
+        self.assertIn(gp2_group, self.test_record._install_modules())
+        self.test_record.managing_target = "individual"
+        self.assertIn(gp2_individual, self.test_record._install_modules())
+
+    @patch("odoo.addons.base.models.ir_module.Module.button_immediate_install")
+    def test_09_action_done(self, mock_button_immediate_install):
+        mock_button_immediate_install.return_value = None
+        action = self.test_record.action_done()
+        self.assertEqual(
+            action,
+            {
+                "type": "ir.actions.client",
+                "tag": "reload",
+            },
+        )
