fd7f10fa2f2cf30028e0f0ad4fa8f8881a34eaec
red <redick@newlogic.com>
2024-09-04 15:06:43 +0800

    improve test case in spp_auto_update_entitlements

--- spp_auto_update_entitlements/tests/__init__.py
+++ spp_auto_update_entitlements/tests/__init__.py
@@ -1,2 +1,3 @@
+from . import common
 from . import test_cycle
 from . import test_entitlement
--- spp_auto_update_entitlements/tests/test_cycle.py
+++ spp_auto_update_entitlements/tests/test_cycle.py
@@ -1,41 +1,14 @@
 from datetime import timedelta
 
 from odoo import fields
-from odoo.tests.common import TransactionCase
+
+from .common import Common
 
 
-class TestCycle(TransactionCase):
+class TestCycle(Common):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.program = cls.env["g2p.program"].create({"name": "Test Program"})
-        cls.partner = cls.env["res.partner"].create({"name": "Test Partner"})
-
-        cls.cycle = cls.env["g2p.cycle"].create(
-            {
-                "name": "Test Cycle",
-                "program_id": cls.program.id,
-                "start_date": fields.Datetime.now(),
-                "end_date": fields.Datetime.now(),
-            }
-        )
-
-        cls.cycle_manager = cls.env["g2p.cycle.manager.default"].create(
-            {
-                "name": "Test Cycle Manager",
-                "program_id": cls.program.id,
-            }
-        )
-
-        cls.entitlement = cls.env["g2p.entitlement"].create(
-            {
-                "program_id": cls.program.id,
-                "partner_id": cls.partner.id,
-                "cycle_id": cls.cycle.id,
-                "state": "draft",
-                "initial_amount": 100.00,
-            }
-        )
 
         cls.entitlement_transaction = cls.env["spp.entitlement.transactions"].create(
             {
