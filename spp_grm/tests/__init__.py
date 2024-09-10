7a7a6a6bc0f8856481992d2460731a25137527b2
Edwin Gonzales <edwin@newlogic.com>
2024-09-10 16:43:27 +0800

    Renamed the test files

--- spp_grm/tests/__init__.py
+++ spp_grm/tests/__init__.py
@@ -1,3 +1,3 @@
-from . import grm_ticket
-from . import grm_ticket_stage
-from . import res_partner
+from . import test_grm_ticket
+from . import test_grm_ticket_stage
+from . import test_res_partner
