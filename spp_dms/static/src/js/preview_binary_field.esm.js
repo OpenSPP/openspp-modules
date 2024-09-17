61fdc5b7854e98ad4b875330c46d8431074aa2d1
Edwin Gonzales <edwin@newlogic.com>
2024-09-17 20:23:23 +0800

    Fix issue with loading PDF

--- spp_dms/static/src/js/preview_binary_field.esm.js
+++ spp_dms/static/src/js/preview_binary_field.esm.js
@@ -8,6 +8,7 @@ import {useService} from "@web/core/utils/hooks";
 export class PreviewRecordWidget extends Component {
     setup() {
         this.store = useService("mail.store");
+        this.orm = useService("orm");
         this.fileViewer = useFileViewer();
     }
 
@@ -15,15 +16,40 @@ export class PreviewRecordWidget extends Component {
         ev.preventDefault();
         ev.stopPropagation();
 
-        const self = this;
-        const attachment = this.store.Attachment.insert({
-            id: self.props.record.resId,
-            filename: self.props.record.data.name || "",
-            name: self.props.record.data.name || "",
-            mimetype: self.props.record.data.mimetype,
-            model_name: self.props.record.resModel,
-        });
-        this.fileViewer.open(attachment);
+        const resId = this.props.record.resId;
+        const resModel = this.props.record.resModel;
+
+        const [record] = await this.orm.read(resModel, [resId], ["content", "name", "mimetype"]);
+
+        if (!record || !record.content) {
+            console.error("No file found!");
+            return;
+        }
+
+        const binaryData = atob(record.content);
+        const arrayBuffer = new Uint8Array(binaryData.length);
+
+        for (let i = 0; i < binaryData.length; i++) {
+            arrayBuffer[i] = binaryData.charCodeAt(i);
+        }
+
+        const blob = new Blob([arrayBuffer], {type: record.mimetype});
+        const fileUrl = URL.createObjectURL(blob);
+
+        if (record.mimetype === "application/pdf") {
+            window.open(fileUrl, "_blank");
+        } else {
+            const attachment = this.store.Attachment.insert({
+                id: resId,
+                filename: record.name || "",
+                name: record.name || "",
+                mimetype: record.mimetype,
+                model_name: resModel,
+                url: fileUrl,
+            });
+
+            this.fileViewer.open(attachment);
+        }
     }
 }
 
