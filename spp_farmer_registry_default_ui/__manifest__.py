3e7605aaa5ace64bcbfd4bb96012186f9c440245
red <redick@newlogic.com>
2024-09-17 19:41:00 +0800

    moved the UI of spp_farmer_registry_demo to the new module named spp_farmer_registry_default_ui

---
+++ spp_farmer_registry_default_ui/__manifest__.py
@@ -0,0 +1,28 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+
+
+{
+    "name": "OpenSPP Farmer Registry Default UI",
+    "summary": "Provides UI for Farmer Registry Base.",
+    "category": "OpenSPP",
+    "version": "17.0.1.0.0",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/OpenSPP/openspp-modules",
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "maintainers": ["reichie020212"],
+    "depends": [
+        "spp_farmer_registry_base",
+    ],
+    "data": [
+        "views/group_view.xml",
+        "views/individual_view.xml",
+    ],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
