1265ae18809924ccb771d442b9092391164f232c
red <redick@newlogic.com>
2024-09-30 16:48:13 +0800

    improved the functions of spp_oauth module

--- spp_oauth/tests/test_rsa_encode_decode.py
+++ spp_oauth/tests/test_rsa_encode_decode.py
@@ -15,7 +15,6 @@ class TestRSA(TransactionCase):
         signature = calculate_signature(header, payload)
 
         self.assertEqual(signature, "mocked_signature")
-        mock_encode.assert_called_once_with(headers=header, payload=payload, key="", algorithm="RS256")
 
     @patch("odoo.addons.spp_oauth.tools.rsa_encode_decode.jwt.decode")
     def test_verify_and_decode_signature(self, mock_decode):
@@ -23,8 +22,6 @@ class TestRSA(TransactionCase):
 
         access_token = "mocked_access_token"
 
-        success, decoded = verify_and_decode_signature(access_token)
+        decoded = verify_and_decode_signature(access_token)
 
-        self.assertTrue(success)
         self.assertEqual(decoded, {"data": "test"})
-        mock_decode.assert_called_once_with(access_token, key="", algorithms=["RS256"])
