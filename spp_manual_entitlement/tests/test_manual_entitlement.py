a2233b4a6ea56fc794f002efa29d808bd294e273
emjay0921 <michael@newlogic.com>
2024-10-10 16:41:50 +0000

    improve test coverage for spp_manual_entitlement

--- spp_manual_entitlement/tests/test_manual_entitlement.py
+++ spp_manual_entitlement/tests/test_manual_entitlement.py
@@ -1,8 +1,24 @@
+import base64
+import os
+
 from odoo import _
+from odoo.exceptions import UserError
 from odoo.tests.common import TransactionCase
 
 
 class TestManualEntitlement(TransactionCase):
+    @staticmethod
+    def get_file_path_1():
+        return f"{os.path.dirname(os.path.abspath(__file__))}/test_entitlement.xlsx"
+
+    @staticmethod
+    def get_file_path_2():
+        return f"{os.path.dirname(os.path.abspath(__file__))}/test_entitlement.csv"
+
+    @staticmethod
+    def get_file_path_3():
+        return f"{os.path.dirname(os.path.abspath(__file__))}/test_entitlement.pdf"
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -11,6 +27,7 @@ class TestManualEntitlement(TransactionCase):
                 "name": "Registrant 1 [MANUAL ENTITLEMENT TEST]",
                 "is_registrant": True,
                 "is_group": True,
+                "spp_id": "GRP_VKW59J49",
             }
         )
         cls.registrant_2 = cls.env["res.partner"].create(
@@ -18,6 +35,7 @@ class TestManualEntitlement(TransactionCase):
                 "name": "Registrant 2 [MANUAL ENTITLEMENT TEST]",
                 "is_registrant": True,
                 "is_group": True,
+                "spp_id": "GRP_VKW59J48",
             }
         )
         cls.program_wizard = cls.env["g2p.program.create.wizard"].create(
@@ -70,6 +88,19 @@ class TestManualEntitlement(TransactionCase):
         wizard.create_entitlement()
         return wizard
 
+    def prepare_entitlement_wizard_for_manual(self, cycle):
+        wizard = self.env["spp.manual.entitlement.wizard"].create(
+            {
+                "cycle_id": cycle.id,
+                "step": "step1",
+                "cycle_membership_ids": [
+                    (0, 0, {"partner_id": self.registrant_1.id}),
+                    (0, 0, {"partner_id": self.registrant_2.id}),
+                ],
+            }
+        )
+        return wizard
+
     def test_02_prepare_entitlement_manual(self):
         cycle = self.set_cycle()
         wizard = self.prepare_entitlement_wizard(cycle)
@@ -100,3 +131,83 @@ class TestManualEntitlement(TransactionCase):
         cycle_manager.prepare_manual_entitlements(cycle, wizard.cycle_membership_ids)
         self.assertEqual(cycle.locked, True)
         self.assertEqual(cycle.locked_reason, _("Prepare entitlement for beneficiaries."))
+
+    def test_06_entitlement_wizard_wrong_file(self):
+        pdf_file = None
+        pdf_file_name = None
+
+        file_path = self.get_file_path_3()
+        with open(file_path, "rb") as f:
+            pdf_file_name = f.name
+            pdf_file = base64.b64encode(f.read())
+
+        cycle = self.set_cycle()
+        wizard = self.prepare_entitlement_wizard_for_manual(cycle)
+        wizard.write({"file": pdf_file, "filename": pdf_file_name})
+        with self.assertRaisesRegex(UserError, _("Only Excel and CSV files are allowed!")):
+            wizard.file_change()
+
+    def test_07_entitlement_wizard_xlsx(self):
+        cycle = self.set_cycle()
+        wizard = self.prepare_entitlement_wizard_for_manual(cycle)
+        wizard.step_upload_csv()
+        self.assertEqual(wizard.step, "step2b_1")
+
+        xls_file = None
+        xls_file_name = None
+
+        file_path = self.get_file_path_1()
+        with open(file_path, "rb") as f:
+            xls_file_name = f.name
+            xls_file = base64.b64encode(f.read())
+
+        wizard.write({"file": xls_file, "filename": xls_file_name})
+        wizard.start_import()
+        self.assertEqual(len(wizard.upload_cycle_membership_ids), 2)
+
+        wizard.finalize_import()
+        self.assertEqual(wizard.step, "step3")
+
+        wizard.create_entitlement()
+        self.assertTrue(wizard.search_existing_entitlement(self.registrant_1.id), "Correct value")
+        self.assertTrue(wizard.search_existing_entitlement(self.registrant_2.id), "Correct value")
+
+    def test_08_entitlement_wizard_csv(self):
+        cycle = self.set_cycle()
+        wizard = self.prepare_entitlement_wizard_for_manual(cycle)
+        wizard.step_upload_csv()
+        self.assertEqual(wizard.step, "step2b_1")
+
+        csv_file = None
+        csv_file_name = None
+
+        file_path = self.get_file_path_1()
+        with open(file_path, "rb") as f:
+            csv_file_name = f.name
+            csv_file = base64.b64encode(f.read())
+
+        wizard.write({"file": csv_file, "filename": csv_file_name})
+        wizard.start_import()
+        self.assertEqual(len(wizard.upload_cycle_membership_ids), 2)
+
+        wizard.finalize_import()
+        self.assertEqual(wizard.step, "step3")
+
+        wizard.create_entitlement()
+        self.assertTrue(wizard.search_existing_entitlement(self.registrant_1.id), "Correct value")
+        self.assertTrue(wizard.search_existing_entitlement(self.registrant_2.id), "Correct value")
+
+    def test_09_manual_add_entitlements(self):
+        cycle = self.set_cycle()
+        wizard = self.prepare_entitlement_wizard_for_manual(cycle)
+        wizard.step_manual_select()
+        self.assertEqual(wizard.step, "step2a")
+
+        wizard.cycle_membership_ids[0].selected = True
+        wizard.cycle_membership_ids[1].selected = True
+        wizard.step_input_entitlement_amounts()
+        self.assertEqual(wizard.step, "step3")
+
+        wizard.create_entitlement()
+        self.assertTrue(wizard.search_existing_entitlement(self.registrant_1.id), "Correct value")
+        self.assertTrue(wizard.search_existing_entitlement(self.registrant_2.id), "Correct value")
