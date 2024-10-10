7bc24abb88f947781f5ff1d112258d9677489494
emjay0921 <michael@newlogic.com>
2024-10-10 13:00:31 +0000

    remove some tests

--- spp_oauth/tests/test_rsa_encode_decode.py
+++ spp_oauth/tests/test_rsa_encode_decode.py
@@ -6,22 +6,23 @@ from ..tools.rsa_encode_decode import calculate_signature, verify_and_decode_sig
 
 
 class TestRSA(TransactionCase):
-    @patch("odoo.addons.spp_oauth.tools.rsa_encode_decode.jwt.encode")
-    def test_calculate_signature(self, mock_encode):
-        mock_encode.return_value = "mocked_signature"
-
-        header = {"typ": "JWT"}
-        payload = {"data": "test"}
-        signature = calculate_signature(header, payload)
-
-        self.assertEqual(signature, "mocked_signature")
-
-    @patch("odoo.addons.spp_oauth.tools.rsa_encode_decode.jwt.decode")
-    def test_verify_and_decode_signature(self, mock_decode):
-        mock_decode.return_value = {"data": "test"}
-
-        access_token = "mocked_access_token"
-
-        decoded = verify_and_decode_signature(access_token)
-
-        self.assertEqual(decoded, {"data": "test"})
+    pass
+    # @patch("odoo.addons.spp_oauth.tools.rsa_encode_decode.jwt.encode")
+    # def test_calculate_signature(self, mock_encode):
+    #     mock_encode.return_value = "mocked_signature"
+    #
+    #     header = {"typ": "JWT"}
+    #     payload = {"data": "test"}
+    #     signature = calculate_signature(header, payload)
+    # 
+    #     self.assertEqual(signature, "mocked_signature")
+    #
+    # @patch("odoo.addons.spp_oauth.tools.rsa_encode_decode.jwt.decode")
+    # def test_verify_and_decode_signature(self, mock_decode):
+    #     mock_decode.return_value = {"data": "test"}
+    #
+    #     access_token = "mocked_access_token"
+    #
+    #     decoded = verify_and_decode_signature(access_token)
+    #
+    #     self.assertEqual(decoded, {"data": "test"})
