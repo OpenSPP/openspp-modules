e5e3c3b21003b00b324bec42610aa92da1e12499
emjay0921 <michael@newlogic.com>
2024-10-11 04:49:57 +0000

    fix spp_import_match tests

--- spp_import_match/tests/test_res_partner_import_match.py
+++ spp_import_match/tests/test_res_partner_import_match.py
@@ -1,19 +1,46 @@
 import logging
-from os import path
+import os
 
 from odoo.tests import TransactionCase
 
 _logger = logging.getLogger(__name__)
 
-PATH = path.join(path.dirname(__file__), "import_data", "%s.csv")
 OPTIONS = {
-    "headers": True,
-    "quoting": '"',
+    "import_skip_records": [],
+    "import_set_empty_fields": [],
+    "fallback_values": {},
+    "name_create_enabled_fields": {},
+    "encoding": "ascii",
     "separator": ",",
+    "quoting": '"',
+    "date_format": "",
+    "datetime_format": "",
+    "float_thousand_separator": ",",
+    "float_decimal_separator": ".",
+    "advanced": True,
+    "has_headers": True,
+    "keep_matches": False,
+    "limit": 2000,
+    "sheets": [],
+    "sheet": "",
+    "skip": 0,
+    "tracking_disable": True,
 }
 
 
 class TestResPartnerImportMatch(TransactionCase):
+    @staticmethod
+    def get_file_path_1():
+        return f"{os.path.dirname(os.path.abspath(__file__))}/res_partner_group_name.csv"
+
+    @staticmethod
+    def get_file_path_excel_1():
+        return f"{os.path.dirname(os.path.abspath(__file__))}/res_partner_group_name.xls"
+
+    @staticmethod
+    def get_file_path_2():
+        return f"{os.path.dirname(os.path.abspath(__file__))}/res_partner_name.csv"
+
     def setUp(self):
         super().setUp()
         self._test_hh = self.env["res.partner"].create(
@@ -36,20 +63,24 @@ class TestResPartnerImportMatch(TransactionCase):
             }
         )
 
-    def _base_import_record(self, res_model, file_name):
-        with open(PATH % file_name) as demo_file:
-            return self.env["base_import.import"].create(
-                {
-                    "res_model": res_model,
-                    "file": demo_file.read(),
-                    "file_name": "%s.csv" % file_name,
-                    "file_type": "csv",
-                }
-            )
+    def _base_import_record(self, res_model, file_path):
+        with open(file_path, encoding="utf-8") as f:
+            csv_file = str.encode(f.read(), "utf-8")
+            csv_file_name = f.name
+
+        base_import = self.env["base_import.import"].create(
+            {
+                "res_model": res_model,
+                "file": csv_file,
+                "file_name": csv_file_name,
+                "file_type": "csv",
+            }
+        )
+        return base_import
 
     def create_matching_given_family_name(self):
         res_partner = self.env["ir.model"].search([("model", "=", "res.partner")])
-        vals = {"model_id": res_partner.id}
+        vals = {"model_id": res_partner.id, "overwrite_match": True}
         import_match = self.env["spp.import.match"].create(vals)
         given_name_field = self.env["ir.model.fields"].search(
             [("name", "=", "given_name"), ("model_id", "=", res_partner.id)]
@@ -67,7 +98,7 @@ class TestResPartnerImportMatch(TransactionCase):
 
     def create_matching_name(self):
         res_partner = self.env["ir.model"].search([("model", "=", "res.partner")])
-        vals = {"model_id": res_partner.id}
+        vals = {"model_id": res_partner.id, "overwrite_match": True}
         import_match = self.env["spp.import.match"].create(vals)
         name_field = self.env["ir.model.fields"].search([("name", "=", "name"), ("model_id", "=", res_partner.id)])
 
@@ -75,24 +106,22 @@ class TestResPartnerImportMatch(TransactionCase):
 
         return import_match
 
-    # Failing Tests
-    # TODO: Fix these test cases
-    # def test_01_res_partner_change_email_by_name(self):
-    #     """Change email based on given_name, family_name."""
-    #     self.create_matching_given_family_name()
-    #     record = self._base_import_record("res.partner", "res_partner_name")
+    def test_01_res_partner_change_email_by_name(self):
+        """Change email based on given_name, family_name."""
+        self.create_matching_given_family_name()
+        file_path = self.get_file_path_2()
+        record = self._base_import_record("res.partner", file_path)
+        record.execute_import(["given_name", "family_name", "name", "email"], [], OPTIONS)
 
-    #     record.execute_import(["given_name", "family_name", "name", "email"], [], OPTIONS)
+        self._test_applicant.env.cache.invalidate()
+        self.assertEqual(self._test_applicant.email, "rufinorenaud@gmail.com")
 
-    #     self._test_applicant.env.cache.invalidate()
-    #     self.assertEqual(self._test_applicant.email, "rufinorenaud@gmail.com")
+    def test_02_res_partner_change_email_by_group_name(self):
+        """Change email based on name."""
+        self.create_matching_name()
+        file_path = self.get_file_path_1()
+        record = self._base_import_record("res.partner", file_path)
 
-    # def test_02_res_partner_change_email_by_group_name(self):
-    #     """Change email based on name."""
-    #     self.create_matching_name()
-    #     record = self._base_import_record("res.partner", "res_partner_group_name")
-
-    #     record.execute_import(["name", "email"], [], OPTIONS)
-
-    #     self._test_hh.env.cache.invalidate()
-    #     self.assertEqual(self._test_hh.email, "renaudhh@gmail.com")
+        record.execute_import(["name", "email"], ["name", "email"], OPTIONS)
+        self._test_hh.env.cache.invalidate()
+        self.assertEqual(self._test_hh.email, "renaudhh@gmail.com")
