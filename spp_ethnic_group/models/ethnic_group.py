3d22d4af63a0a8674f661132c36278a746334f1c
red <redick@newlogic.com>
2024-10-12 15:09:57 +0800

    added new module named spp_spmis_dswd4ps; improved spp_ethnic_group module

--- spp_ethnic_group/models/ethnic_group.py
+++ spp_ethnic_group/models/ethnic_group.py
@@ -7,3 +7,16 @@ class OpenSPPEthnicGroup(models.Model):
 
     name = fields.Char("Ethnic Group", required=True)
     ethnic_group_id = fields.Char("Code", required=True)
+
+    _sql_constraints = [
+        (
+            "name_uniq",
+            "unique(name)",
+            "Name must be unique!",
+        ),
+        (
+            "ethnic_group_id_uniq",
+            "unique(ethnic_group_id)",
+            "Code must be unique!",
+        ),
+    ]
---
+++ spp_spmis_dswd4ps/models/area.py
@@ -0,0 +1,11 @@
+from odoo import fields, models
+
+
+class OpenSPPArea(models.Model):
+    _inherit = "spp.area"
+
+    gida = fields.Selection(
+        string="GIDA?",
+        selection=[("yes", "Yes"), ("no", "No"), ("undetermined", "Undetermined")],
+        default="undetermined",
+    )
