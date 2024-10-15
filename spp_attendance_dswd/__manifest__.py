dce93ed8ade9baedfa4f7e0837c8466454e2d7ae
red <redick@newlogic.com>
2024-10-15 11:38:41 +0800

    created new module named spp_attendance_dswd

---
+++ spp_attendance_dswd/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "OpenSPP Attendance: DSWD",
+    "category": "OpenSPP",
+    "version": "17.0.1.0.0",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/OpenSPP/openspp-modules",
+    "license": "LGPL-3",
+    "development_status": "Alpha",
+    "maintainers": ["reichie020212"],
+    "depends": ["spp_attendance"],
+    "data": [
+        "data/attendance_type_data.xml",
+    ],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
