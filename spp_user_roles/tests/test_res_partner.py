39007b99ffe6987e51811d8f4ddfd0330dce269a
Edwin Gonzales <edwin@newlogic.com>
2024-10-17 10:52:50 +0800

    Fix errors in tests

--- spp_user_roles/tests/test_res_partner.py
+++ spp_user_roles/tests/test_res_partner.py
@@ -18,23 +18,11 @@ class ResPartnerTest(TransactionCase):
         cls.partner_2 = cls.env["res.partner"].create({"name": "Partner 2", "area_id": cls.center_area_2.id})
 
     def _mock_center_area_ids(self):
-        """Helper to mock the center_area_ids."""
+        """Helper to mock the center_area_ids as a recordset."""
+        mock_recordset = self.env["spp.area"].browse([self.center_area_1.id])
         return patch(
-            "odoo.addons.spp_user_roles.models.user.ResUsersCustomSPP.center_area_ids", new=[self.center_area_1.id]
-        )
-
-    def _check_partner_search_result(self, result, expected_partner):
-        """Reusable assertion to validate the search result."""
-        self.assertEqual(len(result), 1)
-        self.assertEqual(
-            result,
-            [
-                {
-                    "id": expected_partner.id,
-                    "name": expected_partner.name,
-                    "area_id": (expected_partner.area_id.id, expected_partner.area_id.draft_name),
-                }
-            ],
+            "odoo.addons.spp_user_roles.models.user.ResUsersCustomSPP.center_area_ids",
+            new_callable=lambda: mock_recordset,
         )
 
     def test_search_read(self):
@@ -55,11 +43,25 @@ class ResPartnerTest(TransactionCase):
         partners = self.env["res.partner"]
 
         # Test the standard web_search_read behavior
-        result = partners.web_search_read(domain=[], specification=[])
+        result = partners.web_search_read(domain=[], specification={"fields": ["id", "name", "area_id"]})
         self.assertEqual(len(result["records"]), partners.search_count([]))
 
         # Test web_search_read with mocked area
         with self._mock_center_area_ids():
-            result = partners.web_search_read(domain=[], specification=[])
+            result = partners.web_search_read(domain=[], specification={"fields": ["id", "name", "area_id"]})
             self.assertEqual(len(result["records"]), 1)
             self.assertEqual(result["records"][0]["id"], self.partner_1.id)
+
+    def _check_partner_search_result(self, result, expected_partner):
+        """Reusable assertion to validate the search result."""
+        self.assertEqual(len(result), 1)
+        self.assertEqual(
+            result,
+            [
+                {
+                    "id": expected_partner.id,
+                    "name": expected_partner.name,
+                    "area_id": (expected_partner.area_id.id, expected_partner.area_id.draft_name),
+                }
+            ],
+        )
