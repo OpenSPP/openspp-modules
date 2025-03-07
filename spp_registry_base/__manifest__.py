7ffa914effd716c7a11ccd9ba74afddf45cc3368
red <redick@newlogic.com>
2025-03-07 15:29:56 +0800

    [ADD] spp_registry_base: add icon

--- spp_registry_base/__manifest__.py
+++ spp_registry_base/__manifest__.py
@@ -24,6 +24,7 @@
         "security/ir.model.access.csv",
         "views/groups_view.xml",
         "views/individuals_view.xml",
+        "views/main_view.xml",
     ],
     "assets": {
         "web.assets_backend": [
