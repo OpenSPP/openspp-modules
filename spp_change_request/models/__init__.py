f9e3058378db0f1479d5befd82bec2f3b8c1bf13
Edwin Gonzales <edwin@newlogic.com>
2025-03-11 09:08:59 +0800

    [UPD] spp_change_request: Initial commit

--- spp_change_request/models/__init__.py
+++ spp_change_request/models/__init__.py
@@ -1,10 +1,6 @@
 # Part of OpenSPP. See LICENSE file for full copyright and licensing details.
 
+from . import change_request_targets
 from . import group_membership
 from . import change_request
-from . import change_request_stage
-from . import dms_file
-from . import dms_directory
-from . import registry
 from . import mixins
-from . import change_request_targets
