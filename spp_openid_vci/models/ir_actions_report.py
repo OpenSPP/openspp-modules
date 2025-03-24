48376b845b11ba555b69fdc82b3039df112d5fd3
red <redick@newlogic.com>
2025-03-24 10:49:48 +0800

    [REF] spp_openid_vci: refactor code

--- spp_openid_vci/models/ir_actions_report.py
+++ spp_openid_vci/models/ir_actions_report.py
@@ -4,8 +4,11 @@ from odoo import models
 class IrActionsReport(models.Model):
     _inherit = "ir.actions.report"
 
+    def _get_logo_url(self):
+        web_base_url = self.env["ir.config_parameter"].sudo().get_param("web.base.url", default="")
+        return f"{web_base_url}/spp_openid_vci/static/description/icon.png"
+
     def _get_rendering_context(self, report, docids, data):
         data = super()._get_rendering_context(report, docids, data)
-        web_base_url = self.env["ir.config_parameter"].sudo().get_param("web.base.url", default="")
-        data["logo_url"] = f"{web_base_url}/spp_openid_vci/static/description/icon.png"
+        data["logo_url"] = self._get_logo_url()
         return data
