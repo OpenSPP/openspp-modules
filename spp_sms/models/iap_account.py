4cb2b5eded478aa28abcbdbc91c1be25f37d3bca
Red <redickbutay02@gmail.com>
2025-04-11 13:27:04 +0800

    [REF] spp_sms: refactor sms sending

--- spp_sms/models/iap_account.py
+++ spp_sms/models/iap_account.py
@@ -1,5 +1,21 @@
 # Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+import logging
+
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest import Client
+
 from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
+
+TWILIO_ERROR_STATES = {
+    21211: "invalid_to_number",
+    21212: "invalid_from_number",
+    21214: "cannot_be_reached",
+    63038: "reached_rate_limit",
+    21659: "invalid_from_number_mismatch",
+    21267: "invalid_from_number_alphanumeric",
+}
 
 
 class IapAccount(models.Model):
@@ -20,3 +36,63 @@ class IapAccount(models.Model):
 
     active_status = fields.Boolean(string="Active", default=True)
     sequence = fields.Integer(default=1)
+
+    def twilio_send_sms_batch(self, messages):
+        _logger.info("SMS Provider: %s" % self.provider)
+        account_id = self.sms_twilio_account_id
+        account_token = self.sms_twilio_token_id
+        account_from = self.sms_twilio_from
+        client = Client(account_id, account_token)
+        result = []
+        for message in messages:
+            content = message["content"]
+            numbers = message["numbers"]
+            for number in numbers:
+                state = "error"
+                send_message = None
+                try:
+                    send_message = client.messages.create(
+                        to=number["number"],
+                        from_=account_from,
+                        body=content,
+                    )
+                    _logger.info("Twilio SNS: %s" % send_message)
+                    state = "success"
+                except TwilioRestException as e:
+                    _logger.error("Twilio SNS: %s" % e)
+                    state = TWILIO_ERROR_STATES.get(e.code, "error")
+                result.append({"state": state, "credit": 0, "res_id": number["number"]})
+        return result
+
+    def sns_amazon_send_sms_batch(self, messages):
+        _logger.info("SMS Provider: %s" % self.provider)
+        result = []
+        for message in messages:
+            numbers = message["numbers"]
+            for number in numbers:
+                result.append({"state": "sns_amazon_error", "credit": 0, "res_id": number["number"]})
+        return result
+
+        # NOTE: using Amazon SNS will result in an error for now since boto3 is not
+        # compatible with the latest version of urllib3
+
+        # account_id = self.sns_amazon_key
+        # account_secret = self.sns_amazon_secret
+        # account_region = self.sns_amazon_region
+        # sns_client = boto3.client(
+        #     "sns",
+        #     aws_access_key_id=sns_key,
+        #     aws_secret_access_key=sns_secret,
+        #     region_name=sns_region,
+        # )
+
+        # message = sns_client.publish(
+        #     PhoneNumber=messages[0]["number"],
+        #     Message=messages[0]["content"],
+        # )
+        # state = "error"
+        # if message:
+        #     state = "success"
+
+        # _logger.info("Amazon SNS: %s" % message)
+        # return [{"state": state, "credit": 0, "res_id": messages[0]["res_id"]}]
--- spp_sms/tools/sms_api.py
+++ spp_sms/tools/sms_api.py
@@ -1,8 +1,5 @@
 import logging
 
-from twilio.base.exceptions import TwilioRestException
-from twilio.rest import Client
-
 from odoo import _
 
 from odoo.addons.sms.tools.sms_api import SmsApi
@@ -18,82 +15,29 @@ original_get_sms_api_error_messages = SmsApi._get_sms_api_error_messages
 
 def _send_sms_batch(self, messages, delivery_reports_url=False):
     account = self.env["iap.account"].search(
-        [("active_status", "=", True), ("provider", "=", "sms_twilio")], order_by="sequence", limit=1
+        [("active_status", "=", True), ("service_name", "=", "sms")], order="sequence", limit=1
     )
 
-    if not account or account[0].provider != "sms_twilio":
+    if not account or account.provider not in ["sms_twilio", "sns_amazon"]:
         return original_send_sms_batch(self, messages, delivery_reports_url)
 
-    _logger.info("SMS Provider: %s" % account[0].provider)
-
-    account_id = account[0].sms_twilio_account_id
-    account_token = account[0].sms_twilio_token_id
-    account_from = account[0].sms_twilio_from
-    client = Client(account_id, account_token)
-    result = []
-    for message in messages:
-        content = message["content"]
-        numbers = message["numbers"]
-        for number in numbers:
-            state = "error"
-            send_message = None
-            try:
-                send_message = client.messages.create(
-                    to=number["number"],
-                    from_=account_from,
-                    body=content,
-                )
-                _logger.info("Twilio SNS: %s" % send_message)
-                state = "success"
-            except TwilioRestException as e:
-                _logger.error("Twilio SNS: %s" % e)
-                TWILIO_ERROR_STATES = {
-                    21211: "invalid_to_number",
-                    21212: "invalid_from_number",
-                    21214: "cannot_be_reached",
-                    63038: "reached_rate_limit",
-                    21659: "invalid_from_number_mismatch",
-                    21267: "invalid_from_number_alphanumeric",
-                }
-
-                state = TWILIO_ERROR_STATES.get(e.code, "error")
-            result.append({"state": state, "credit": 0, "res_id": number["number"]})
-    return result
-
-    # NOTE: Removing sns_amazon for now since boto3 is not compatible with the current version of urllib3
-    # elif account[0].provider == "sns_amazon":
-    #     sns_key = account[0].sns_amazon_key
-    #     sns_secret = account[0].sns_amazon_secret
-    #     sns_region = account[0].sns_amazon_region
-    #     sns_client = boto3.client(
-    #         "sns",
-    #         aws_access_key_id=sns_key,
-    #         aws_secret_access_key=sns_secret,
-    #         region_name=sns_region,
-    #     )
-
-    #     message = sns_client.publish(
-    #         PhoneNumber=messages[0]["number"],
-    #         Message=messages[0]["content"],
-    #     )
-    #     state = "error"
-    #     if message:
-    #         state = "success"
-
-    #     _logger.info("Amazon SNS: %s" % message)
-    #     return [{"state": state, "credit": 0, "res_id": messages[0]["res_id"]}]
+    elif account.provider == "sms_twilio":
+        return account.twilio_send_sms_batch(messages)
+    elif account.provider == "sns_amazon":
+        return account.sns_amazon_send_sms_batch(messages)
 
 
 def _get_sms_api_error_messages(self):
     error_messages = original_get_sms_api_error_messages(self)
-    error_messages["invalid_to_number"] = _("The number you're trying to reach is not correctly formatted.")
-    error_messages["invalid_from_number"] = _("The number you're trying to send from is not correctly formatted.")
-    error_messages["cannot_be_reached"] = _("The number you're trying to reach is not correctly formatted.")
+    error_messages["invalid_to_number"] = _("The number cannot be reached. Please check the number.")
+    error_messages["invalid_from_number"] = _("The number you're trying to use is not correctly formatted.")
+    error_messages["cannot_be_reached"] = _("The number cannot be reached. Please check the number.")
     error_messages["reached_rate_limit"] = _("The number you're trying to reach has reached the rate limit.")
     error_messages["invalid_from_number_mismatch"] = _(
         "'From' number is not a Twilio phone number or Short Code country mismatch"
     )
     error_messages["invalid_from_number_alphanumeric"] = _("Alphanumeric Sender ID cannot be used as the 'From' number")
+    error_messages["sns_amazon_error"] = _("Amazon SNS is not yet supported.")
     return error_messages
 
 
