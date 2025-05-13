1debbbb8a3232a611a73e37486153def3c8067c5
Edwin Gonzales <edwin@newlogic.com>
2025-05-13 11:11:39 +0800

    Initial commit

---
+++ spp_registry_donor/__manifest__.py
@@ -0,0 +1,36 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "OpenSPP Registry Donor",
+    "summary": "OpenSPP Donor Registry Management",
+    "category": "OpenSPP",
+    "version": "17.0.1.0.0",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/OpenSPP/openspp-modules",
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "maintainers": ["gonzalesedwin1123"],
+    "depends": [
+        "base",
+        "g2p_registry_base",
+        "g2p_registry_individual",
+        "spp_user_roles",
+    ],
+    "data": [
+        # Security
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        
+        # Views
+        "views/res_partner_views.xml",
+        
+        # Data
+        "data/user_roles.xml",
+    ],
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
