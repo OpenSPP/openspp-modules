6f1709ab494520702ecb61fc2f8a2bcbd74592e5
emjay0921 <michael@newlogic.com>
2025-09-01 13:20:27 +0800

    [ADD] add new settings for oauth pub and priv keys

---
+++ spp_oauth/models/res_config_settings.py
@@ -0,0 +1,13 @@
+from odoo import _, api, fields, models
+
+class RegistryConfig(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    oauth_priv_key = fields.Char(
+        string="OAuth Private Key",
+        config_parameter="spp_oauth.oauth_priv_key",
+    )
+    oauth_pub_key = fields.Char(
+        string="OAuth Public Key",
+        config_parameter="spp_oauth.oauth_pub_key",
+    )
