36d0ead50588452f722b91008fd4982a7761d49b
emjay0921 <michael@newlogic.com>
2025-09-01 13:22:06 +0800

    [ADD] modifications on parameter to hide parameter

---
+++ spp_oauth/models/ir_config_parameter.py
@@ -0,0 +1,7 @@
+from odoo import _, api, fields, models
+
+
+class ConfigParameter(models.Model):
+    _inherit = "ir.config_parameter"
+
+    hidden = fields.Boolean(string="Hidden", default=False, help="If checked, this parameter will be hidden in the settings UI.")
