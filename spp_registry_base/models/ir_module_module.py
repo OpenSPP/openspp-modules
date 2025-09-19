eed08791279777c35b94698486e2cd7592d67c09
emjay0921 <michael@newlogic.com>
2025-09-19 13:11:44 +0800

    [IMP] add logger to check the status

--- spp_registry_base/models/ir_module_module.py
+++ spp_registry_base/models/ir_module_module.py
@@ -1,4 +1,7 @@
+import logging
 from odoo import models, api
+
+_logger = logging.getLogger(__name__)
 
 class IrModuleModule(models.Model):
     _inherit = 'ir.module.module'
@@ -18,7 +21,16 @@ class IrModuleModule(models.Model):
         for module in self:
             icon_info = self.ICON_MAP.get(module.name)
             if icon_info:
-                menu = self.env.ref(icon_info['menu_xml_id'], raise_if_not_found=False)
+                menu = self.env['ir.ui.menu'].search([('xml_id', '=', icon_info['menu_xml_id'])], limit=1)
                 if menu:
                     menu.write({'web_icon': icon_info['icon']})
+                    _logger.info(
+                        "Updated icon for menu '%s' (module '%s') to '%s'",
+                        icon_info['menu_xml_id'], module.name, icon_info['icon']
+                    )
+                else:
+                    _logger.warning(
+                        "Menu with xml_id '%s' not found for module '%s'",
+                        icon_info['menu_xml_id'], module.name
+                    )
         return res
