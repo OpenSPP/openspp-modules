511666a75bd8f279f80ecc8dbb2c1e16c6f89281
emjay0921 <michael@newlogic.com>
2025-09-29 10:52:21 +0800

    [ADD] logger to check what is causing the error

--- spp_area_base/models/area_import.py
+++ spp_area_base/models/area_import.py
@@ -250,7 +250,7 @@ class OpenSPPAreaImport(models.Model):
             # Fallback to xlrd for .xls files
             _logger.info("Opening .xls file: %s", filename)
             try:
-                _logger.info("InputX: %s", inputx)
+                _logger.info("InputX: %s", inputx.seek(0))
                 book = open_workbook(inputx)
                 return book
             except Exception as e:
