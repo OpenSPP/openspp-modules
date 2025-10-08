c1a5a3b27b4e3c9badead21fb1fb6e8d8b7b472e
emjay0921 <michael@newlogic.com>
2025-10-08 16:51:49 +0800

    [FIX] import

--- spp_change_request_base/tests/__init__.py
+++ spp_change_request_base/tests/__init__.py
@@ -1,5 +1,4 @@
-from . import test_models
 from . import test_change_requests
 from . import test_source_mixin
 from . import test_spp_dms_file
-from . import test_wizards
+from . import test_wizard
