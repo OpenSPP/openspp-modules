f097602e10208f417c81ee79ca0f4fe03f9c4881
emjay0921 <michael@newlogic.com>
2025-10-10 16:51:28 +0800

    [IMP] generator

--- spp_demo_common/models/res_partner.py
+++ spp_demo_common/models/res_partner.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 
-class SPPResPartner(models.TransientModel):
+class SPPResPartner(models.Model):
     _inherit = "res.partner"
 
     demo_data_generator_id = fields.Many2one("spp.demo.data.generator", string="Demo Data Generator", readonly=True)
