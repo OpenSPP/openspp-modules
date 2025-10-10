fe4a8df198ed4d0042aa2f54b3a371ab9d1070f1
emjay0921 <michael@newlogic.com>
2025-10-10 14:45:39 +0800

    [FIX] checking of existing

--- spp_demo_common/models/data_import.py
+++ spp_demo_common/models/data_import.py
@@ -16,7 +16,7 @@ class SPPDataImporter(models.Model):
     _description = "SPP Data Importer"
 
     SKIP_FIELDS = ["message_partner_ids", "age"]
-    DOMAIN_FIELDS = ["name", "code", "value", "phone_no", "email", "display_name", "group", "individual"]
+    DOMAIN_FIELDS = ["name", "code", "value", "phone_no", "email", "display_name", "group", "individual", "login"]
 
     def _default_queue_job_minimum_size(self):
         default_settings = self.env["ir.config_parameter"].sudo()
@@ -560,17 +560,17 @@ class SPPDataImporter(models.Model):
             json_data = self._check_skip_fields(json_data)
 
             model = self.env[raw.model_name]
+            
+            # Build creation data
+            creation_data = self._build_creation_data(raw, json_data, model, created_mapping, _creating)
 
             # Check if record already exists
-            existing_id = self._check_existing_record(raw, json_data, model, created_mapping, raw_ref)
+            existing_id = self._check_existing_record(raw, creation_data, model, created_mapping, raw_ref)
             if existing_id:
                 # Save the mapping
                 self.created_raw_mapping_json = json.dumps(created_mapping)
                 return existing_id
-
-            # Build creation data
-            creation_data = self._build_creation_data(raw, json_data, model, created_mapping, _creating)
-
+            
             # Create the record
             _logger.info(f"Creating {raw.model_name} with data: {creation_data}")
             new_record = model.create(creation_data)
@@ -603,16 +603,17 @@ class SPPDataImporter(models.Model):
         finally:
             _creating.discard(raw.id)
 
-    def _check_existing_record(self, raw, json_data, model, created_mapping, raw_ref):
+    def _check_existing_record(self, raw, creation_data, model, created_mapping, raw_ref):
         """Check if record already exists based on common identifying fields."""
         possible_fields = self.DOMAIN_FIELDS
         domain = []
         for field in possible_fields:
-            if field in json_data and field in model._fields:
+            if field in creation_data and field in model._fields:
                 # Check if field is a stored field
                 if not model._fields[field].store:
                     continue
-                domain.append((field, "=", json_data[field]))
+
+                domain.append((field, "=", creation_data[field]))
         _logger.info(f"Checking existing record for raw {raw.id} with domain: {domain}")
         if domain:
             existing = model.search(domain, limit=1)
@@ -625,22 +626,8 @@ class SPPDataImporter(models.Model):
                     }
                 )
                 created_mapping[raw_ref] = existing.id
+                self.created_raw_mapping_json = json.dumps(created_mapping)
                 _logger.info(f"Skipped creation for raw {raw.id}, record already exists with ID {existing.id}")
-                return existing.id
-            
-        # Special case for res.users
-        if raw.model_name == "res.users" and "login" in json_data:
-            existing = model.search([("login", "=", json_data["login"])], limit=1)
-            if existing:
-                raw.write(
-                    {
-                        "state": "saved",
-                        "db_id": existing.id,
-                        "remarks": "User already exists, skipped creation.",
-                    }
-                )
-                created_mapping[raw_ref] = existing.id
-                _logger.info(f"Skipped creation for raw {raw.id}, user already exists with ID {existing.id}")
                 return existing.id
 
         return None
@@ -685,6 +672,8 @@ class SPPDataImporter(models.Model):
 
             creation_data[field_name] = value
 
+        # Do a recheck of existing records based on identifying fields
+        existing_id = self._check_existing_record(raw, creation_data, model, created_mapping, raw_ref)
         return creation_data
 
     def _create_process_many2many_field(self, field_name, value, created_mapping, _creating):
