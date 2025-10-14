e34d965a33688e90e5282b77a783715161618a06
emjay0921 <michael@newlogic.com>
2025-10-14 11:18:44 +0800

    [FIX] make spmis base an app

--- spp_base_spmis/__manifest__.py
+++ spp_base_spmis/__manifest__.py
@@ -58,7 +58,7 @@
     "assets": {},
     "demo": [],
     "images": [],
-    "application": False,
+    "application": True,
     "installable": True,
     "auto_install": False,
     "summary": "Establishes core system settings and generates unique identifiers for records, enhancing registrant profiles with attributes like tags, gender, and type. The module also integrates Top-up Cards for identification and provides fundamental user interface elements with initial security configurations for user roles.",
