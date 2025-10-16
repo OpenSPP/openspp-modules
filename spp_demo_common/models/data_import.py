75117b5004376264d9a86532648e29bff2c8e8cb
emjay0921 <michael@newlogic.com>
2025-10-16 10:43:50 +0800

    [FIX] apps wizard

--- spp_demo_common/models/data_import.py
+++ spp_demo_common/models/data_import.py
@@ -894,7 +894,7 @@ class SPPDataImporter(models.Model):
                     models.append(data.get("model", ""))
                 self.model_list = ", ".join(models)
 
-                module_names = [name.strip() for name in modules.split(",") if name.strip()]
+                module_names = self.module_list
                 not_installed_modules = self.env["ir.module.module"].search(
                     [
                         ("name", "in", module_names),
