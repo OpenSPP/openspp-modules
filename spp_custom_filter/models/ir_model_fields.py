f055412fa962a29591d72ff12cb77bd2b53f2d04
emjay0921 <michael@newlogic.com>
2025-10-17 11:27:51 +0800

    [ADD] filter targeting

--- spp_custom_filter/models/ir_model_fields.py
+++ spp_custom_filter/models/ir_model_fields.py
@@ -9,14 +9,22 @@ class IrModelFields(models.Model):
         default=False,
         help="Allow to show this field on Custom filter!",
     )
+    filter_target = fields.Selection(
+        [("both", "Both"), ("group", "Group"), ("individual", "Individual")],
+        string="Filter Target",
+        default="both",
+    )
 
     def _reflect_field_params(self, field, model_id):
         vals = super()._reflect_field_params(field, model_id)
         vals["allow_filter"] = getattr(field, "allow_filter", False)
+        vals["filter_target"] = getattr(field, "filter_target", "both")
         return vals
 
     def _instanciate_attrs(self, field_data):
         attrs = super()._instanciate_attrs(field_data)
         if attrs and field_data.get("allow_filter"):
             attrs["allow_filter"] = field_data["allow_filter"]
+        if attrs and field_data.get("filter_target"):
+            attrs["filter_target"] = field_data["filter_target"]
         return attrs
