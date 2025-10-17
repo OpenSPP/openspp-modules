8382ada97c27069eba7287af80a78e5bc3b39209
emjay0921 <michael@newlogic.com>
2025-10-17 12:27:35 +0800

    [FIX] remove _get_view

--- spp_custom_filter/models/custom_filter_mixin.py
+++ spp_custom_filter/models/custom_filter_mixin.py
@@ -49,7 +49,6 @@ class CustomFilterMixin(models.AbstractModel):
         if self.user_has_groups("base.group_no_one"):
             return res
         context = self.env.context.copy()
-        _logger.info("Custom Filter Mixin Context: %s", context)
         for fname in res.keys():
             if fname == "id":
                 allow_filter = getattr(self._fields[fname], "allow_filter", True)
@@ -73,49 +72,6 @@ class CustomFilterMixin(models.AbstractModel):
                 res[fname]["exportable"] = allow_filter and res[fname]["exportable"]
 
         return res
-
-    @api.model
-    def _get_view(self, view_id=None, view_type='form', **options):
-        res = super()._get_view(view_id=view_id, view_type=view_type, **options)
-        context = self.env.context.copy()
-        _logger.info("Custom Filter Mixin _get_view Context: %s", context)
-        # Log the options to see if they contain relevant info
-        _logger.info("Custom Filter Mixin _get_view Options: %s", options)
-        # Log the result to see its structure
-        _logger.info("Custom Filter Mixin _get_view Result: %s", res)
-        
-        if isinstance(res, tuple):
-            view_arch, fields = res
-        else:
-            view_arch = res.get('arch')
-            fields = res.get('fields', {})
-
-        if isinstance(fields, dict):
-            for fname, field in fields.items():
-                if fname == "id":
-                    allow_filter = getattr(self._fields[fname], "allow_filter", True)
-                    filter_target = getattr(self._fields[fname], "filter_target", "both")
-                else:
-                    allow_filter = getattr(self._fields[fname], "allow_filter", False)
-                    filter_target = getattr(self._fields[fname], "filter_target", "both")
-
-                if filter_target == "individual" and context.get("is_group"):
-                    allow_filter = False
-                if filter_target == "group" and not context.get("is_group"):
-                    allow_filter = False
-                if filter_target == "both":
-                    allow_filter = allow_filter
-
-                if field.get("searchable"):
-                    field["searchable"] = allow_filter and field["searchable"]
-                if field.get("exportable"):
-                    field["exportable"] = allow_filter and field["exportable"]
-
-        if isinstance(res, tuple):
-            return (view_arch, fields)
-        else:
-            res['fields'] = fields
-            return res
     
     def _valid_field_parameter(self, field, name):
         return name in ["allow_filter", "filter_target"] or super()._valid_field_parameter(field, name)
