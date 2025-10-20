ce20f8458b325053e185f346f3b9adead1fe1335
emjay0921 <michael@newlogic.com>
2025-10-20 10:59:59 +0800

    [FIX] Duplicate Relation error message

--- spp_base_common/models/relationship.py
+++ spp_base_common/models/relationship.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -19,6 +19,40 @@ class SPPRegistrantRelationship(models.Model):
                     "Relation field required. To complete registrant relation, "
                     "please select a relation type from the dropdown. If no options are available, "
                     "go to Configuration > Relation Types to create relation types first."
+                )
+
+    @api.constrains("source", "relation", "destination", "start_date", "end_date")
+    def _check_relation_uniqueness(self):
+        """Forbid multiple active relations of the same type between the same
+        partners
+        :raises ValidationError: When constraint is violated
+        """
+        for record in self:
+            domain = [
+                ("relation", "=", record.relation.id),
+                ("id", "!=", record.id),
+                ("source", "=", record.source.id),
+                ("destination", "=", record.destination.id),
+            ]
+            if record.start_date:
+                domain += [
+                    "|",
+                    ("end_date", "=", False),
+                    ("end_date", ">=", record.start_date),
+                ]
+            if record.end_date:
+                domain += [
+                    "|",
+                    ("start_date", "=", False),
+                    ("start_date", "<=", record.end_date),
+                ]
+            if record.search(domain):
+                raise ValidationError(
+                    _(
+                        "Duplicate Relation Detected !\n\n"
+                        "A record with the same relation type already exists and has overlapping dates. \n\n"
+                        "To save multiple entries of the same relation, please define distinct start and end dates."
+                    )
                 )
 
     @api.constrains("relation")
