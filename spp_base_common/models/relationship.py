30ec95640320e94c33cfe66871908aa050e094c4
emjay0921 <michael@newlogic.com>
2025-10-20 09:46:01 +0800

    [ADD] constraints on relation

--- spp_base_common/models/relationship.py
+++ spp_base_common/models/relationship.py
@@ -1,7 +1,24 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 
 class SPPRegistrantRelationship(models.Model):
     _inherit = "g2p.reg.rel"
 
     relation_inverse = fields.Char(related="relation.name_inverse", string="Relation")
+
+    def _validate_relation_required(self):
+        """Validate that relation is required"""
+        for record in self:
+            if not record.relation:
+                raise ValidationError("Registrant Relationship's relation is required")
+
+    @api.onchange("relation")
+    def _onchange_relation(self):
+        """Validate that relation is required"""
+        self._validate_relation_required()
+
+    @api.constrains("relation")
+    def _check_relation_required(self):
+        """Validate that relation is required on save"""
+        self._validate_relation_required()
