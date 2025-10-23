56a0dce03ad4cc59cda31e8bf653794be19c0a04
emjay0921 <michael@newlogic.com>
2025-10-23 13:54:40 +0800

    [FIX] head duplication

--- spp_demo_common/models/demo_data_generator.py
+++ spp_demo_common/models/demo_data_generator.py
@@ -147,7 +147,6 @@ class SPPDemoDataGenerator(models.Model):
         new_group_name = False
         for _ in range(num_members):
             head_membership = self.head_member_getter(group)
-            have_head_member = bool(head_membership)
             is_head_member = random.choice([True, False]) if not have_head_member else False
 
             # Check if last member and no head member assigned yet
