f117f7b04809efe66a74c777d36e18819a9f07bc
emjay0921 <michael@newlogic.com>
2025-10-27 15:24:58 +0800

    [FIX] view context

--- spp_base_farmer_registry_demo/__manifest__.py
+++ spp_base_farmer_registry_demo/__manifest__.py
@@ -37,6 +37,7 @@
         "data/feed_items_data.xml",
         "views/group_view.xml",
         "views/individual_view.xml",
+        "views/generator_view.xml",
     ],
     "assets": {},
     "demo": [],
