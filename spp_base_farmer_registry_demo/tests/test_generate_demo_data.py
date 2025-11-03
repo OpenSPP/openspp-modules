0f205e5acef5f515c82f25f7170802312c84f305
emjay0921 <michael@newlogic.com>
2025-11-03 17:59:50 +0800

    [FIX] test

--- spp_base_farmer_registry_demo/tests/test_generate_demo_data.py
+++ spp_base_farmer_registry_demo/tests/test_generate_demo_data.py
@@ -26,7 +26,7 @@ class TestFarmerRegistryDemoDataGenerator(TransactionCase):
         cls.test_country = cls.env["res.country"].create(
             {
                 "name": "Test Country Farmer Registry",
-                "code": "TF2",
+                "code": "T2",
                 "faker_locale": "en_US",
                 "faker_locale_available": True,
                 "lat_min": -10.0,
