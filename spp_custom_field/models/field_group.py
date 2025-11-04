60f6cb9d4e9d668e6a6bdd94173aa0565e2a1cab
emjay0921 <michael@newlogic.com>
2025-11-04 13:21:38 +0800

    [IMP] UI and fields

--- spp_custom_field/models/field_group.py
+++ spp_custom_field/models/field_group.py
@@ -9,6 +9,13 @@ class CustomFieldGroup(models.Model):
     _order = "sequence, name"
 
     name = fields.Char(string="Group Name", required=True, translate=True)
+    target_type = fields.Selection(
+        selection=[("grp", "Group"), ("indv", "Individual")],
+        string="Target Type",
+        required=True,
+        default="grp",
+        help="Specify whether this group is for Group or Individual fields",
+    )
     sequence = fields.Integer(string="Sequence", default=10)
     description = fields.Text(string="Description", translate=True)
     active = fields.Boolean(string="Active", default=True)
