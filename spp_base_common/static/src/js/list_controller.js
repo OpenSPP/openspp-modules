34b5a50d70795e62505446c8583e15c42aa44058
emjay0921 <michael@newlogic.com>
2025-11-07 16:32:39 +0800

    [FIX] swap parameter

--- spp_base_common/static/src/js/list_controller.js
+++ spp_base_common/static/src/js/list_controller.js
@@ -1,8 +1,5 @@
-/** @odoo-module **/
-
-import { ListController } from "web.ListController";
-import { _t } from "web.core";
-import { session } from "@web/session";
+odoo.define('spp_base_common.ListController', function (require) {
+"use strict";
 
 /**
  * Fix for swapped notification message parameters when archiving/unarchiving records.
@@ -12,7 +9,16 @@ import { session } from "@web/session";
  * 
  * Fixed to show: "Of the 50,000 records selected, only the first 20,000 have been archived"
  * This makes sense - you selected 50,000 but only 20,000 were processed due to active_ids_limit.
+ * 
+ * Reference: https://github.com/odoo/odoo/tree/17.0/odoo/addons/base
  */
+
+var ListController = require('web.ListController');
+var core = require('web.core');
+var session = require('web.session');
+
+var _t = core._t;
+
 ListController.include({
     /**
      * Toggle the archive state of the selected records.
@@ -28,15 +34,19 @@ ListController.include({
         const total = this.model.get(this.handle, {raw: true}).count;
         
         // Fixed: Swapped parameters from (total, resIds.length) to (resIds.length, total)
-        // Original Odoo bug had them backwards in addons/web/static/src/legacy/js/views/list/list_controller.js:658-659
+        // Original Odoo bug at web/static/src/legacy/js/views/list/list_controller.js:658-659
         if (notif && resIds.length === session.active_ids_limit && resIds.length < total) {
             const msg = _.str.sprintf(
                 _t("Of the %d records selected, only the first %d have been archived/unarchived."),
-                resIds.length,  // Fixed: total records selected
-                total           // Fixed: number actually archived/unarchived  
+                total,          // Fixed: total records selected (larger number)
+                resIds.length   // Fixed: number actually archived/unarchived (smaller number)
             );
             this.displayNotification({ title: _t('Warning'), message: msg });
         }
     },
+});
+
+return ListController;
+
 });
 
