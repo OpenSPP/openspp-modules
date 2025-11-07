35fa5309ab1538feab11182a72fbe9e6c07af4c5
emjay0921 <michael@newlogic.com>
2025-11-07 16:55:57 +0800

    [FIX] swap parameter

--- spp_base_common/static/src/legacy/js/views/list/list_controller_fix.js
+++ spp_base_common/static/src/legacy/js/views/list/list_controller_fix.js
@@ -1,41 +1,36 @@
-odoo.define('spp_base_common.ListControllerFix', ['web.ListController', 'web.session', 'web.core'], function (require) {
-'use strict';
+/** @odoo-module **/
 
 /**
  * Patch for Odoo bug: swapped parameters in archive notification message.
  * 
  * Bug location: odoo/addons/web/static/src/legacy/js/views/list/list_controller.js:658-659
- * Bug report: https://github.com/odoo/odoo/issues/xxxxx (if applicable)
+ * Reference: https://github.com/odoo/odoo/tree/17.0/odoo/addons/base
  * 
  * Original shows: "Of the 20,000 records selected, only the first 50,000 have been archived"
  * Fixed to show: "Of the 50,000 records selected, only the first 20,000 have been archived"
  */
 
-var ListController = require('web.ListController');
-var session = require('web.session');
-var core = require('web.core');
+import {ListController} from "@web/views/list/list_controller";
+import {patch} from "@web/core/utils/patch";
 
-var _t = core._t;
-
-ListController.include({
+patch(ListController.prototype, {
     /**
      * @override
      */
-    _toggleArchiveState: async function (archive) {
-        const resIds = await this.getSelectedIdsWithDomain();
-        const notif = this.isDomainSelected;
-        await this._archive(resIds, archive);
-        const total = this.model.get(this.handle, {raw: true}).count;
-        if (notif && resIds.length === session.active_ids_limit && resIds.length < total) {
-            const msg = _.str.sprintf(
-                _t("Of the %d records selected, only the first %d have been archived/unarchived."),
-                resIds.length,  // Fixed: swapped - total selected (larger)
-                total           // Fixed: swapped - actually processed (smaller)  
+    async toggleArchiveState(archive) {
+        const resIds = await this.getSelectedResIds();
+        const total = this.model.root.count;
+        
+        await this.model.root.archive(resIds, archive);
+        
+        if (this.model.root.isDomainSelected && resIds.length < total) {
+            const message = _.str.sprintf(
+                this.env._t("Of the %d records selected, only the first %d have been archived/unarchived."),
+                total,          // Fixed: swapped - total selected (larger)
+                resIds.length   // Fixed: swapped - actually processed (smaller)
             );
-            this.displayNotification({ title: _t('Warning'), message: msg });
+            this.notification.add(message, { type: "warning" });
         }
     },
-});
-
 });
 
