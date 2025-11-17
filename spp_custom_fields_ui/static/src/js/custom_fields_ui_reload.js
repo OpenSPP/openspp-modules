4ab14b5776e83faa53ec20f11d9cf1c121339f54
emjay0921 <michael@newlogic.com>
2025-11-17 11:57:07 +0800

    [IMP] reload handling on creation

--- spp_custom_fields_ui/static/src/js/custom_fields_ui_reload.js
+++ spp_custom_fields_ui/static/src/js/custom_fields_ui_reload.js
@@ -2,21 +2,59 @@
 
 import {FormController} from "@web/views/form/form_controller";
 import {patch} from "@web/core/utils/patch";
+import {useService} from "@web/core/utils/hooks";
 
 patch(FormController.prototype, {
+    setup() {
+        super.setup();
+        this.actionService = useService("action");
+    },
+
     /**
      * Override the saveButtonClicked method to trigger a reload after saving
      * custom fields (ir.model.fields records with target_type).
      */
     async saveButtonClicked(params = {}) {
-        const result = await super.saveButtonClicked(params);
-
         // Check if we're editing ir.model.fields with target_type (custom fields UI)
-        if (this.props.resModel === "ir.model.fields" && this.model.root.data.target_type) {
-            // Reload the page to refresh the model registry
-            window.location.reload();
+        const isCustomField = this.props.resModel === "ir.model.fields" && this.model.root.data.target_type;
+
+        if (!isCustomField) {
+            return super.saveButtonClicked(params);
         }
 
-        return result;
+        // Store record ID before save (will be false for new records)
+        const recordIdBeforeSave = this.model.root.resId;
+
+        // Try to save
+        try {
+            const result = await super.saveButtonClicked(params);
+
+            // Only reload if save was successful
+            // If result is defined and not false, save was successful
+            if (result !== false) {
+                const recordIdAfterSave = this.model.root.resId;
+
+                if (!recordIdBeforeSave && recordIdAfterSave) {
+                    // New record was created - reload and navigate to the saved record
+                    // Use actionService to reload the current action with the new record ID
+                    this.actionService.doAction({
+                        type: "ir.actions.act_window",
+                        res_model: this.props.resModel,
+                        res_id: recordIdAfterSave,
+                        views: [[false, "form"]],
+                        target: "current",
+                    });
+                } else if (recordIdBeforeSave) {
+                    // Existing record was edited - just reload the page
+                    window.location.reload();
+                }
+            }
+
+            return result;
+        } catch (error) {
+            // Save failed (validation error, required fields missing, etc.)
+            // Don't reload, let the user fix the errors
+            throw error;
+        }
     },
 });
