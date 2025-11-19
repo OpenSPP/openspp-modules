0d32b3ed81983f70937e9cb138396b4152df84bd
emjay0921 <michael@acn.fr>
2025-11-19 16:22:49 +0800

    refactor: standardize menu icon naming to openspp-icons-menu- pattern
    - Renamed all icon-* files to openspp-icons-menu-* for consistency
    - Updated icon references in ir_module_module.py ICON_MAP
    - Follows kebab-case naming convention throughout
    - All menu icons now use the standard openspp-icons-menu-{menuname} pattern

--- spp_base_common/models/ir_module_module.py
+++ spp_base_common/models/ir_module_module.py
@@ -12,71 +12,71 @@ class IrModuleModule(models.Model):
     ICON_MAP = {
         "project_todo": {
             "menu_xml_id": "project_todo.menu_todo_todos",
-            "icon": "spp_base_common,static/description/icon-To-do-White-line.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-to-do.png",
         },
         "mail": {
             "menu_xml_id": "mail.menu_root_discuss",
-            "icon": "spp_base_common,static/description/icon-Discuss-White-line.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-discuss.png",
         },
         "queue_job": {
             "menu_xml_id": "queue_job.menu_queue_job_root",
-            "icon": "spp_base_common,static/description/icon-Job-Queue-White-line.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-job-queue.png",
         },
         "spreadsheet_dashboard": {
             "menu_xml_id": "spreadsheet_dashboard.spreadsheet_dashboard_menu_root",
-            "icon": "spp_base_common,static/description/icon-Dashboards-White-line.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-dashboards.png",
         },
         "project": {
             "menu_xml_id": "project.menu_main_pm",
-            "icon": "spp_base_common,static/description/icon-Project-White-line.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-project.png",
         },
         "mass_mailing": {
             "menu_xml_id": "mass_mailing.mass_mailing_menu_root",
-            "icon": "spp_base_common,static/description/icon-Email-Marketing-White-line.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-email-marketing.png",
         },
         "survey": {
             "menu_xml_id": "survey.menu_surveys",
-            "icon": "spp_base_common,static/description/icon-Surveys-White-line.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-surveys.png",
         },
         "hr": {
             "menu_xml_id": "hr.menu_hr_root",
-            "icon": "spp_base_common,static/description/icon-Employees-White-line.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-employees.png",
         },
         "calendar": {
             "menu_xml_id": "calendar.mail_menu_calendar",
-            "icon": "spp_base_common,static/description/OpenSPP-Icons-Menu-Calendar.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-calendar.png",
         },
         "contacts": {
             "menu_xml_id": "contacts.menu_contacts",
-            "icon": "spp_base_common,static/description/OpenSPP-Icons-Menu-Contacts.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-contacts.png",
         },
         "account": {
             "menu_xml_id": "account.menu_finance",
-            "icon": "spp_base_common,static/description/OpenSPP-Icons-Menu-Invoicing.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-invoicing.png",
         },
         "event": {
             "menu_xml_id": "event.event_main_menu",
-            "icon": "spp_base_common,static/description/OpenSPP-Icons-Menu-Events.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-events.png",
         },
         "stock": {
             "menu_xml_id": "stock.menu_stock_root",
-            "icon": "spp_base_common,static/description/OpenSPP-Icons-Menu-Inventory.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-inventory.png",
         },
         "utm": {
             "menu_xml_id": "utm.menu_link_tracker_root",
-            "icon": "spp_base_common,static/description/OpenSPP-Icons-Menu-Link-Tracker.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-link-tracker.png",
         },
         "fastapi": {
             "menu_xml_id": "fastapi.menu_fastapi_root",
-            "icon": "spp_base_common,static/description/icon-fast-api.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-fast-api.png",
         },
         "spp_grm": {
             "menu_xml_id": "spp_grm.spp_grm_ticket_main_menu",
-            "icon": "spp_base_common,static/description/Helpdesk.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-helpdesk.png",
         },
         "point_of_sale": {
             "menu_xml_id": "point_of_sale.menu_point_root",
-            "icon": "spp_base_common,static/description/Point_of_Sale.png",
+            "icon": "spp_base_common,static/description/openspp-icons-menu-point-of-sale.png",
         },
     }
 
